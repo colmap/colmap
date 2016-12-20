3bf13fb22c8efe4e887b334991496195b8c17110
Johannes Schönberger <jsch@demuc.de>
2016-12-20 11:42:02 +0100

    Add script to read COLMAP model in Python

---
+++ scripts/python/read_model.py
@@ -0,0 +1,119 @@
+# COLMAP - Structure-from-Motion and Multi-View Stereo.
+# Copyright (C) 2016  Johannes L. Schoenberger <jsch at inf.ethz.ch>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import sys
+import collections
+
+import numpy as np
+
+
+def read_cameras(path):
+    cameras = {}
+    Camera = collections.namedtuple(
+        "Camera", ["id", "model", "width", "height", "params"])
+    with open(path, "r") as fid:
+        while True:
+            line = fid.readline()
+            if not line:
+                break
+            line = line.strip()
+            if len(line) > 0 and line[0] != "#":
+                elems = line.split()
+                camera_id = int(elems[0])
+                model = elems[1]
+                width = int(elems[2])
+                height = int(elems[3])
+                params = np.array(tuple(map(float, elems[4:])))
+                cameras[camera_id] = Camera(id=camera_id, model=model,
+                                            width=width, height=height,
+                                            params=params)
+    return cameras
+
+
+def read_images(path):
+    images = {}
+    Image = collections.namedtuple(
+        "Image", ["id", "qvec", "tvec", "camera_id", "name", "xys",
+                  "point3D_ids"])
+    with open(path, "r") as fid:
+        while True:
+            line = fid.readline()
+            if not line:
+                break
+            line = line.strip()
+            if len(line) > 0 and line[0] != "#":
+                elems = line.split()
+                image_id = int(elems[0])
+                qvec = np.array(tuple(map(float, elems[1:5])))
+                tvec = np.array(tuple(map(float, elems[5:8])))
+                camera_id = int(elems[8])
+                image_name = elems[9]
+                elems = fid.readline().split()
+                xys = np.column_stack([tuple(map(float, elems[0::3])),
+                                       tuple(map(float, elems[1::3]))])
+                point3D_ids = np.array(tuple(map(int, elems[2::3])))
+                images[image_id] = Image(id=image_id, qvec=qvec, tvec=tvec,
+                                         camera_id=camera_id, name=image_name,
+                                         xys=xys, point3D_ids=point3D_ids)
+    return images
+
+
+def read_points3D(path):
+    points3D = {}
+    Point3D = collections.namedtuple(
+        "Point3D", ["id", "xyz", "rgb", "error", "image_ids", "point2D_idxs"])
+    with open(path, "r") as fid:
+        while True:
+            line = fid.readline()
+            if not line:
+                break
+            line = line.strip()
+            if len(line) > 0 and line[0] != "#":
+                elems = line.split()
+                point3D_id = int(elems[0])
+                xyz = np.array(tuple(map(float, elems[1:4])))
+                rgb = np.array(tuple(map(int, elems[4:7])))
+                error = float(elems[7])
+                image_ids = np.array(tuple(map(int, elems[8::2])))
+                point2D_idxs = np.array(tuple(map(int, elems[9::2])))
+                points3D[point3D_id] = Point3D(id=point3D_id, xyz=xyz, rgb=rgb,
+                                               error=error, image_ids=image_ids,
+                                               point2D_idxs=point2D_idxs)
+    return points3D
+
+
+def read_model(path):
+    cameras = read_cameras(os.path.join(path, "cameras.txt"))
+    images = read_images(os.path.join(path, "images.txt"))
+    points3D = read_points3D(os.path.join(path, "points3D.txt"))
+    return cameras, images, points3D
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python read_model.py path/to/model/folder/")
+        return
+
+    cameras, images, points3D = read_model(sys.argv[1])
+
+    print("num_cameras:", len(cameras))
+    print("num_images:", len(images))
+    print("num_points3D:", len(points3D))
+
+
+if __name__ == '__main__':
+    main()
