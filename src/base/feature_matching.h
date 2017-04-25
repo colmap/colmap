28001904eaa27273a44dbac0d4ac095d893e4d72
Johannes Schönberger <jsch@demuc.de>
2017-04-25 13:26:17 -0400

    Change default number of maximum matches

--- src/base/feature_matching.h
+++ src/base/feature_matching.h
@@ -53,7 +53,7 @@ struct SiftMatchingOptions {
   bool cross_check = true;
 
   // Maximum number of matches.
-  int max_num_matches = 32768;
+  int max_num_matches = 16384;
 
   // Maximum epipolar error in pixels for geometric verification.
   double max_error = 4.0;
