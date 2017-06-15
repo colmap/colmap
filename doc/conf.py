391b5cca5f25dfc2daca92a780120365bc5402de
Johannes Schönberger <jsch@demuc.de>
2017-06-15 14:05:08 +0200

    Increase version number to 3.1

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,7 @@ copyright = u'2017, Johannes L. Schoenberger'
 # built documents.
 #
 # The short X.Y version.
-version = '3.0'
+version = '3.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
