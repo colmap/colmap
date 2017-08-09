fea27fdfa1f9bc2440c8085233369895d960ce7a
Johannes Schönberger <jsch@demuc.de>
2017-08-09 09:52:54 +0200

    Some minor modifications to Python model reading script

--- scripts/python/read_model.py
+++ scripts/python/read_model.py
@@ -41,22 +41,28 @@ camera_models = {
     CameraModel(model_id=9, model_name="FOV", num_params=5),
     CameraModel(model_id=10, model_name="THIN_PRISM_FISHEYE", num_params=12)
 }
-cam_model_id_to_model = dict([(cam_model.model_id, cam_model) for cam_model in camera_models])
+camera_model_ids = dict([(cam_model.model_id, cam_model) \
+                         for cam_model in camera_models])
 
 
-def read_next_bytes(binary_file, num_bytes, format_char_sequence, endian_character='<'):
+def read_next_bytes(fid, num_bytes, format_char_sequence, endian_character="<"):
+    """Read and unpack the next bytes from a binary file.
+    :param fid:
+    :param num_bytes: Sum of combination of {2, 4, 8}, e.g. 2, 6, 16, 30, etc.
+    :param format_char_sequence: List of {c, e, f, d, h, H, i, I, l, L, q, Q}.
+    :param endian_character: Any of {@, =, <, >, !}
+    :return: Tuple of read and unpacked values.
     """
-    :param binary_file:
-    :param num_bytes: combination of {2,4,8}
-    :param format_char_sequence: sequence of {c,e,f,d,h,H,i,I,l,L,q,Q}
-    :param endian_character: {@, =, <, >, !}
-    :return: tuple of corresponding values
-    """
-    binary_data = binary_file.read(num_bytes)
-    return struct.unpack(endian_character + format_char_sequence, binary_data)
+    data = fid.read(num_bytes)
+    return struct.unpack(endian_character + format_char_sequence, data)
 
 
 def read_cameras_text(path):
+    """
+    see: src/base/reconstruction.cc
+        void Reconstruction::WriteCamerasText(const std::string& path)
+        void Reconstruction::ReadCamerasText(const std::string& path)
+    """
     cameras = {}
     with open(path, "r") as fid:
         while True:
@@ -83,21 +89,23 @@ def read_cameras_binary(path_to_model_file):
         void Reconstruction::WriteCamerasBinary(const std::string& path)
         void Reconstruction::ReadCamerasBinary(const std::string& path)
     """
-    with open(path_to_model_file, "rb") as binary_file:
-        num_cameras = read_next_bytes(binary_file, 8, 'Q')[0]
-        cameras = {}
+    cameras = {}
+    with open(path_to_model_file, "rb") as fid:
+        num_cameras = read_next_bytes(fid, 8, "Q")[0]
         for camera_line_index in range(num_cameras):
             camera_properties = read_next_bytes(
-                binary_file, num_bytes=24, format_char_sequence='iiQQ')
+                fid, num_bytes=24, format_char_sequence="iiQQ")
             camera_id = camera_properties[0]
-            camera_model_id = camera_properties[1]
+            model_id = camera_properties[1]
+            model_name = camera_model_ids[camera_properties[1]].model_name
             width = camera_properties[2]
             height = camera_properties[3]
-            num_params = cam_model_id_to_model[camera_model_id].num_params
-            params = read_next_bytes(
-                binary_file, num_bytes=8*num_params, format_char_sequence='d'*num_params)
+            num_params = camera_model_ids[model_id].num_params
+            params = read_next_bytes(fid, num_bytes=8*num_params,
+                                     format_char_sequence="d"*num_params)
+
             cameras[camera_id] = Camera(id=camera_id,
-                                        model=cam_model_id_to_model[camera_model_id].model_name,
+                                        model=model_name,
                                         width=width,
                                         height=height,
                                         params=np.array(params))
@@ -106,6 +114,11 @@ def read_cameras_binary(path_to_model_file):
 
 
 def read_images_text(path):
+    """
+    see: src/base/reconstruction.cc
+        void Reconstruction::ReadImagesText(const std::string& path)
+        void Reconstruction::WriteImagesText(const std::string& path)
+    """
     images = {}
     with open(path, "r") as fid:
         while True:
@@ -137,31 +150,30 @@ def read_images_binary(path_to_model_file):
         void Reconstruction::ReadImagesBinary(const std::string& path)
         void Reconstruction::WriteImagesBinary(const std::string& path)
     """
-    with open(path_to_model_file, "rb") as binary_file:
-        num_reg_images = read_next_bytes(binary_file, 8, 'Q')[0]
-        images = {}
+    images = {}
+    with open(path_to_model_file, "rb") as fid:
+        num_reg_images = read_next_bytes(fid, 8, "Q")[0]
         for image_index in range(num_reg_images):
             binary_image_properties = read_next_bytes(
-                binary_file,
+                fid,
                 num_bytes=64,   # 7*8+2*4
-                format_char_sequence='idddddddi')
+                format_char_sequence="idddddddi")
 
             image_id = binary_image_properties[0]
             qvec = np.array(binary_image_properties[1:5])
             tvec = np.array(binary_image_properties[5:8])
             camera_id = np.array(binary_image_properties[8])
 
-            # Read the image name
-            current_char = read_next_bytes(binary_file, 1, 'c')[0]
-            image_name = ''
-            while current_char != b'\x00':   # look for the ASCII 0 entry
+            image_name = ""
+            current_char = read_next_bytes(fid, 1, "c")[0]
+            while current_char != b"\x00":   # look for the ASCII 0 entry
                 image_name += current_char.decode("utf-8")
-                current_char = read_next_bytes(binary_file, 1, 'c')[0]
+                current_char = read_next_bytes(fid, 1, "c")[0]
 
-            num_points2D = read_next_bytes(
-                binary_file, num_bytes=8, format_char_sequence='Q')[0]
-            x_y_id_s = read_next_bytes(
-                binary_file, num_bytes=24*num_points2D, format_char_sequence='ddq'*num_points2D)
+            num_points2D = read_next_bytes(fid, num_bytes=8,
+                                           format_char_sequence="Q")[0]
+            x_y_id_s = read_next_bytes(fid, num_bytes=24*num_points2D,
+                                       format_char_sequence="ddq"*num_points2D)
             xys = np.column_stack([tuple(map(float, x_y_id_s[0::3])),
                        tuple(map(float, x_y_id_s[1::3]))])
             point3D_ids = np.array(tuple(map(int, x_y_id_s[2::3])))
@@ -173,6 +185,11 @@ def read_images_binary(path_to_model_file):
 
 
 def read_points3D_text(path):
+    """
+    see: src/base/reconstruction.cc
+        void Reconstruction::ReadPoints3DText(const std::string& path)
+        void Reconstruction::WritePoints3DText(const std::string& path)
+    """
     points3D = {}
     with open(path, "r") as fid:
         while True:
@@ -200,12 +217,12 @@ def read_points3d_binary(path_to_model_file):
         void Reconstruction::ReadPoints3DBinary(const std::string& path)
         void Reconstruction::WritePoints3DBinary(const std::string& path)
     """
-    with open(path_to_model_file, "rb") as binary_file:
-        num_points = read_next_bytes(binary_file, 8, 'Q')[0]
-        binary_point_lines = {}
+    points3D = {}
+    with open(path_to_model_file, "rb") as fid:
+        num_points = read_next_bytes(fid, 8, "Q")[0]
         for point_line_index in range(num_points):
             binary_point_line_properties = read_next_bytes(
-                binary_file, num_bytes=43, format_char_sequence='QdddBBBd')
+                fid, num_bytes=43, format_char_sequence="QdddBBBd")
 
             point3D_id = binary_point_line_properties[0]
             xyz = np.array(binary_point_line_properties[1:4])
@@ -213,22 +230,23 @@ def read_points3d_binary(path_to_model_file):
             error = np.array(binary_point_line_properties[7])
 
             track_length = read_next_bytes(
-                binary_file, num_bytes=8, format_char_sequence='Q')[0]
-            binary_point_track_pairs = read_next_bytes(
-                binary_file, num_bytes=8*track_length, format_char_sequence='ii'*track_length)
+                fid, num_bytes=8, format_char_sequence="Q")[0]
+            track_elems = read_next_bytes(
+                fid, num_bytes=8*track_length,
+                format_char_sequence="ii"*track_length)
 
-            image_ids = np.array(tuple(map(int, binary_point_track_pairs[0::2])))
-            point2D_idxs = np.array(tuple(map(int, binary_point_track_pairs[1::2])))
+            image_ids = np.array(tuple(map(int, track_elems[0::2])))
+            point2D_idxs = np.array(tuple(map(int, track_elems[1::2])))
 
-            binary_point_lines[point3D_id] = Point3D(
+            points3D[point3D_id] = Point3D(
                 id=point3D_id, xyz=xyz, rgb=rgb,
                 error=error, image_ids=image_ids,
                 point2D_idxs=point2D_idxs)
-    return binary_point_lines
+    return points3D
 
 
 def read_model(path, ext):
-    if ext == '.txt':
+    if ext == ".txt":
         cameras = read_cameras_text(os.path.join(path, "cameras" + ext))
         images = read_images_text(os.path.join(path, "images" + ext))
         points3D = read_points3D_text(os.path.join(path, "points3D") + ext)
@@ -241,7 +259,7 @@ def read_model(path, ext):
 
 def main():
     if len(sys.argv) != 3:
-        print("Usage: python read_model.py path/to/model/folder/ ext")
+        print("Usage: python read_model.py path/to/model/folder [.txt,.bin]")
         return
 
     cameras, images, points3D = read_model(path=sys.argv[1], ext=sys.argv[2])
@@ -251,5 +269,5 @@ def main():
     print("num_points3D:", len(points3D))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- scripts/python/test_read_model.py
+++ scripts/python/test_read_model.py
@@ -2,58 +2,64 @@ import numpy as np
 from read_model import read_model
 
 
-def compare_cameras(cameras_1, cameras_2):
-    assert len(cameras_1) == len(cameras_2)
-    for cam_id_1, cam_id_2 in zip(cameras_1, cameras_2):
-        cam_1 = cameras_1[cam_id_1]
-        cam_2 = cameras_2[cam_id_2]
-        assert cam_1.id == cam_2.id
-        assert cam_1.width == cam_2.width
-        assert cam_1.height == cam_2.height
-        assert np.allclose(cam_1.params, cam_2.params)
+def compare_cameras(cameras1, cameras2):
+    assert len(cameras1) == len(cameras2)
+    for camera_id1 in cameras1:
+        camera1 = cameras1[camera_id1]
+        camera2 = cameras2[camera_id1]
+        assert camera1.id == camera2.id
+        assert camera1.width == camera2.width
+        assert camera1.height == camera2.height
+        assert np.allclose(camera1.params, camera2.params)
 
 
-def compare_images(imgs_1, imgs_2):
-    assert len(imgs_1) == len(imgs_2)
-    for img_1_id, img_2_id in zip(imgs_1, imgs_2):
-        img_1 = imgs_1[img_1_id]
-        img_2 = imgs_2[img_2_id]
-        assert img_1.id == img_2.id
-        assert np.allclose(img_1.qvec, img_2.qvec)
-        assert np.allclose(img_1.tvec, img_2.tvec)
-        assert img_1.camera_id == img_2.camera_id
-        assert img_1.name == img_2.name
-        assert np.allclose(img_1.xys, img_2.xys)
-        assert np.array_equal(img_1.point3D_ids, img_2.point3D_ids)
+def compare_images(images1, images2):
+    assert len(images1) == len(images2)
+    for image_id1 in images1:
+        image1 = images1[image_id1]
+        image2 = images2[image_id1]
+        assert image1.id == image2.id
+        assert np.allclose(image1.qvec, image2.qvec)
+        assert np.allclose(image1.tvec, image2.tvec)
+        assert image1.camera_id == image2.camera_id
+        assert image1.name == image2.name
+        assert np.allclose(image1.xys, image2.xys)
+        assert np.array_equal(image1.point3D_ids, image2.point3D_ids)
 
 
-def compare_points(points3D_1, points3D_2):
-    for point_id_1, point_id_2 in zip(points3D_1, points3D_2):
-        point_1 = points3D_1[point_id_1]
-        point_2 = points3D_2[point_id_2]
-        assert point_1.id == point_2.id
-        assert np.allclose(point_1.xyz, point_1.xyz)
-        assert np.array_equal(point_1.rgb, point_1.rgb)
-        assert np.allclose(point_1.error, point_2.error)
-        assert np.array_equal(point_1.image_ids, point_1.image_ids)
-        assert np.array_equal(point_1.point2D_idxs, point_1.point2D_idxs)
+def compare_points(points3D1, points3D2):
+    for point3D_id1 in points3D1:
+        point3D1 = points3D1[point3D_id1]
+        point3D2 = points3D2[point3D_id1]
+        assert point3D1.id == point3D2.id
+        assert np.allclose(point3D1.xyz, point3D1.xyz)
+        assert np.array_equal(point3D1.rgb, point3D1.rgb)
+        assert np.allclose(point3D1.error, point3D2.error)
+        assert np.array_equal(point3D1.image_ids, point3D1.image_ids)
+        assert np.array_equal(point3D1.point2D_idxs, point3D1.point2D_idxs)
 
 
 def main():
     import sys
     if len(sys.argv) != 3:
-        print("Usage: python read_model.py path/to/model/folder/txt path/to/model/folder/bin")
+        print("Usage: python read_model.py "
+              "path/to/model/folder/txt path/to/model/folder/bin")
         return
 
-    print('Comparing text and binary output.')
+    print("Comparing text and binary models ...")
+
     path_to_model_txt_folder = sys.argv[1]
     path_to_model_bin_folder = sys.argv[2]
-    cameras_txt, images_txt, points3D_txt = read_model(path_to_model_txt_folder, ext='.txt')
-    cameras_bin, images_bin, points3D_bin = read_model(path_to_model_bin_folder, ext='.bin')
+    cameras_txt, images_txt, points3D_txt = \
+        read_model(path_to_model_txt_folder, ext=".txt")
+    cameras_bin, images_bin, points3D_bin = \
+        read_model(path_to_model_bin_folder, ext=".bin")
     compare_cameras(cameras_txt, cameras_bin)
     compare_images(images_txt, images_bin)
     compare_points(points3D_txt, points3D_bin)
-    print('Text and binary output are equal.')
 
-if __name__ == '__main__':
+    print("... text and binary models are equal.")
+
+
+if __name__ == "__main__":
     main()
