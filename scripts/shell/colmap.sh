2b73c8de8ab07bcbc278dc99234bf58e36000e8d
Johannes Schoenberger <jsch@demuc.de>
2018-01-04 09:51:03 -0800

    Move colmap executables to lib install directory

--- scripts/shell/colmap.sh
+++ scripts/shell/colmap.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-array_contains() {
-    array=$1
-    value=$2
-    for entry in "${array[@]}"; do
-        if [ "$entry" == "$value" ]; then
-            echo "1"
+check_command() {
+    for c in "${commands[@]}"; do
+        if [ "$command" == "$c" ]; then
+            command_exists=1
             return
         fi
     done
-    echo "0"
+    command_exists=0
 }
 
 show_help() {
+    echo "COLMAP -- Structure-from-Motion and Multi-View Stereo"
+    echo ""
+    echo "Usage:"
+    echo "  colmap [command] [options]"
+    echo ""
     echo "Example usage:"
-    echo "  colmap -h [ --help ]"
+    echo "  colmap help [ -h, --help ]"
     echo "  colmap gui"
     echo "  colmap gui -h [ --help ]"
-    echo "  colmap feature_extractor --project_path PROJECT"
+    echo "  colmap feature_extractor --image_path IMAGES --database_path DATABASE"
+    echo "  colmap exhaustive_matcher --database_path DATABASE"
+    echo "  colmap mapper --image_path IMAGES --database_path DATABASE --export_path EXPORT"
+    echo "  ..."
+    echo ""
+    echo "Documentation:"
+    echo "  https://colmap.github.io/"
     echo ""
     echo "Available commands:"
     for c in "${commands[@]}"
@@ -30,17 +39,21 @@ show_help() {
 }
 
 script_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-colmap_internal_path=$script_path/colmap-internal
-commands=("gui" $(ls $colmap_internal_path))
-
+lib_path=$script_path/../lib/colmap
 command=$1
-command_exists=$(array_contains $commands $command)
+commands=("help" "gui" $(ls $lib_path))
 
-if [ "$command_exists" == "0" ] || [ "$command" == "" ] || [ "$command" == "-h" ] || [ "$command" == "--help" ]; then
+check_command
+
+if [ "$command_exists" == 0 ] || \
+   [ "$command" == "" ] || \
+   [ "$command" == "help" ] || \
+   [ "$command" == "-h" ] || \
+   [ "$command" == "--help" ]; then
     show_help
 elif [ "$command" == "gui" ]; then
     command="colmap"
 fi
 
 shift
-$colmap_internal_path/$command $@
+$lib_path/$command $@
