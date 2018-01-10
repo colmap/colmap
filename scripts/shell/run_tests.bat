75263ffa882412e3bdc255994b5753e3681d5542
Johannes Schoenberger <jsch@demuc.de>
2018-01-09 18:55:41 -0800

    Check for succeeding unit tests in batch script

--- scripts/shell/run_tests.bat
+++ scripts/shell/run_tests.bat
@@ -1,5 +1,12 @@
 set SCRIPT_PATH=%~dp0
 set PATH=%SCRIPT_PATH%\lib;%PATH%
 set QT_PLUGIN_PATH=%SCRIPT_PATH%\lib;%QT_PLUGIN_PATH%
-for %%i in (%SCRIPT_PATH%\*_test.exe) do bin\%%i
+
+for %%i in (%SCRIPT_PATH%\bin\*_test.exe) do (
+    %%i
+
+    if %errorlevel% neq 0 goto end
+)
+
+:end
 pause
