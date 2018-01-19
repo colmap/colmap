35985c5838a37f282fb28c94f4191d425e1fdc6e
Johannes Schoenberger <jsch@demuc.de>
2018-01-18 23:07:12 -0800

    Add Python function to convert rotation matrix to quaternion

--- scripts/python/read_model.py
+++ scripts/python/read_model.py
@@ -267,6 +267,20 @@ def qvec2rotmat(qvec):
          1 - 2 * qvec[1]**2 - 2 * qvec[2]**2]])
 
 
+def rotmat2qvec(R):
+    Rxx, Ryx, Rzx, Rxy, Ryy, Rzy, Rxz, Ryz, Rzz = R.flat
+    K = np.array([
+        [Rxx - Ryy - Rzz, 0, 0, 0],
+        [Ryx + Rxy, Ryy - Rxx - Rzz, 0, 0],
+        [Rzx + Rxz, Rzy + Ryz, Rzz - Rxx - Ryy, 0],
+        [Ryz - Rzy, Rzx - Rxz, Rxy - Ryx, Rxx + Ryy + Rzz]]) / 3.0
+    eigvals, eigvecs = np.linalg.eigh(K)
+    qvec = eigvecs[[3, 0, 1, 2], np.argmax(eigvals)]
+    if qvec[0] < 0:
+        qvec *= -1
+    return qvec
+
+
 def main():
     if len(sys.argv) != 3:
         print("Usage: python read_model.py path/to/model/folder [.txt,.bin]")
