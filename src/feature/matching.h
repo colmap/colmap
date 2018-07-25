e338111b170d957a15fd324aaa5bb57afb169193
Johannes Schoenberger <jsch@demuc.de>
2018-07-25 13:00:33 +0200

    Increase number of images to be matched in sequential matching

--- src/feature/matching.h
+++ src/feature/matching.h
@@ -55,7 +55,7 @@ struct ExhaustiveMatchingOptions {
 
 struct SequentialMatchingOptions {
   // Number of overlapping image pairs.
-  int overlap = 5;
+  int overlap = 10;
 
   // Whether to match images against their quadratic neighbors.
   bool quadratic_overlap = true;
@@ -68,7 +68,7 @@ struct SequentialMatchingOptions {
 
   // The number of images to retrieve in loop detection. This number should
   // be significantly bigger than the sequential matching overlap.
-  int loop_detection_num_images = 30;
+  int loop_detection_num_images = 50;
 
   // Number of nearest neighbors to retrieve per query feature.
   int loop_detection_num_nearest_neighbors = 1;
