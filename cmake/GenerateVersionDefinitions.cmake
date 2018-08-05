e1e07925c5f808094752d0266f17cad58d88c6d5
Johannes Schoenberger <jsch@demuc.de>
2018-08-05 21:53:18 +0200

    Use CMAKE_CURRENT_SOURCE_DIR instead of CMAKE_SOURCE_DIR

--- cmake/GenerateVersionDefinitions.cmake
+++ cmake/GenerateVersionDefinitions.cmake
@@ -29,16 +29,16 @@
 #
 # Author: Johannes L. Schoenberger (jsch at inf.ethz.ch)
 
-if(Git_FOUND AND EXISTS "${CMAKE_SOURCE_DIR}/.git")
+if(Git_FOUND AND EXISTS "${CMAKE_CURRENT_SOURCE_DIR}/.git")
     execute_process(COMMAND
         "${GIT_EXECUTABLE}" rev-parse --short HEAD
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
         OUTPUT_VARIABLE GIT_COMMIT_ID
         ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 
     execute_process(COMMAND
         "${GIT_EXECUTABLE}" log -1 --format=%ad --date=short
-        WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
         OUTPUT_VARIABLE GIT_COMMIT_DATE
         ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
 else()
@@ -46,5 +46,5 @@ else()
     set(GIT_COMMIT_DATE "Unknown")
 endif()
 
-configure_file("${CMAKE_SOURCE_DIR}/src/util/version.h.in"
-               "${CMAKE_SOURCE_DIR}/src/util/version.h")
+configure_file("${CMAKE_CURRENT_SOURCE_DIR}/src/util/version.h.in"
+               "${CMAKE_CURRENT_SOURCE_DIR}/src/util/version.h")
