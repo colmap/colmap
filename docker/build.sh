6f9d3007fb17c623457cf0b950554778f7e69730
Taylor Dahlke <taylor@hivemapper.com>
2019-12-11 11:01:57 -0800

    Update scripts to pull or build the docker container tagged as latest.

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
-docker build -t="colmap:3.6-dev.3" .;
-docker run --gpus all -w /working -v $1:/working -it colmap:3.6-dev.3;
+docker build -t="colmap:latest" .;
+docker run --gpus all -w /working -v $1:/working -it colmap:latest;
