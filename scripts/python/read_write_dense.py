46344e661488af0369fd0a5bf535ccddf809722b
sebastian <sebastian.bullinger@iosb.fraunhofer.de>
2020-08-19 20:46:08 +0200

    Python script for writing depth/normal arrays

--- scripts/python/read_write_dense.py
+++ scripts/python/read_write_dense.py
@@ -34,6 +34,7 @@
 import argparse
 import numpy as np
 import os
+import struct
 
 
 def read_array(path):
@@ -52,6 +53,38 @@ def read_array(path):
         array = np.fromfile(fid, np.float32)
     array = array.reshape((width, height, channels), order="F")
     return np.transpose(array, (1, 0, 2)).squeeze()
+
+
+def write_array(array, path):
+    """
+    see: src/mvs/mat.h
+        void Mat<T>::Write(const std::string& path)
+    """
+    assert array.dtype == np.float32
+    if len(array.shape) == 2:
+        height, width = array.shape
+        channels = 1
+    elif len(array.shape) == 3:
+        height, width, channels = array.shape
+    else:
+        assert False
+
+    with open(path, "w") as fid:
+        fid.write(str(width) + "&" + str(height) + "&" + str(channels) + "&")
+
+    with open(path, "ab") as fid:
+        if len(array.shape) == 2:
+            array_trans = np.transpose(array, (1, 0))
+        elif len(array.shape) == 3:
+            array_trans = np.transpose(array, (1, 0, 2))
+        else:
+            assert False
+        data_1d = array_trans.reshape(-1, order="F")
+        data_list = data_1d.tolist()
+        endian_character = "<"
+        format_char_sequence = "".join(["f"] * len(data_list))
+        byte_data = struct.pack(endian_character + format_char_sequence, *data_list)
+        fid.write(byte_data)
 
 
 def parse_args():
