4916764f7ec46b489ce5093c5ba2c48f1a1b66f4
Sebastian <sebastian.bullinger@iosb.fraunhofer.de>
2020-09-20 08:27:33 +0200

    Python script for writing depth/normal arrays (#957)

---
+++ scripts/python/test_read_write_dense.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2018, ETH Zurich and UNC Chapel Hill.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#
+#     * Neither the name of ETH Zurich and UNC Chapel Hill nor the names of
+#       its contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Johannes L. Schoenberger (jsch-at-demuc-dot-de)
+
+import numpy as np
+from read_write_dense import read_array, write_array
+
+
+def main():
+    import sys
+    if len(sys.argv) != 3:
+        print("Usage: python test_read_write_dense.py "
+              "path/to/dense/input.bin path/to/dense/output.bin")
+        return
+
+    print("Checking consistency of reading and writing dense arrays "
+          + "(depth maps / normal maps) ...")
+
+    path_to_dense_input = sys.argv[1]
+    path_to_dense_output = sys.argv[2]
+
+    dense_input = read_array(path_to_dense_input)
+    print("Input shape: " + str(dense_input.shape))
+
+    write_array(dense_input, path_to_dense_output)
+    dense_output = read_array(path_to_dense_output)
+
+    np.testing.assert_array_equal(dense_input, dense_output)
+
+    print("... dense arrays are equal.")
+
+
+if __name__ == "__main__":
+    main()
