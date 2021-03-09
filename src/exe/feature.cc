0750dea274bc52d8420882bf9cda9716dbbac7dc
Antonios Matakos <amatakos@gmail.com>
2021-03-08 20:41:31 -0800

    Update feature.cc

--- src/exe/feature.cc
+++ src/exe/feature.cc
@@ -63,6 +63,93 @@ bool VerifyCameraParams(const std::string& camera_model,
   return true;
 }
 
+bool UseSpatialMatcher(const std::string& database_path, double max_distance,
+                       bool is_lla) {
+  PrintHeading1("Checking for spatial matching");
+  Database database(database_path);
+  constexpr double limit = std::numeric_limits<double>::max();
+  Eigen::Vector3d min_coord(limit, limit, limit);
+  Eigen::Vector3d max_coord(-limit, -limit, -limit);
+  GPSTransform gps(GPSTransform::WGS84);
+  std::vector<Image> images = database.ReadAllImages();
+  for (const Image& image : database.ReadAllImages()) {
+    if (!image.HasTvecPrior()) {
+      std::cout << "Found image with invalid location prior; using exhaustive "
+                   "matching"
+                << std::endl;
+      return false;
+    }
+    Eigen::Vector3d xyz;
+    if (is_lla) {
+      xyz = gps.EllToXYZ({image.TvecPrior()}).front();
+    } else {
+      xyz = image.TvecPrior();
+    }
+    min_coord = xyz.cwiseMin(min_coord);
+    max_coord = xyz.cwiseMax(max_coord);
+  }
+
+  const double image_distance = (max_coord - min_coord).norm();
+  std::cout << StringPrintf("Max matching distance %f vs max image distance %f",
+                            max_distance, image_distance)
+            << std::endl;
+  if (image_distance > max_distance) {
+    std::cout << "All images have valid location priors and distance; using "
+                 "spatial matching"
+              << std::endl;
+    return true;
+  } else {
+    std::cout << "Max matching distance exceeds max image distance; using "
+                 "exhaustive matching"
+              << std::endl;
+    return false;
+  }
+}
+
+bool UseImagePairsMatcher(const std::string& database_path,
+                          const std::string& match_list_path) {
+  PrintHeading1("Checking for fixed pair matching");
+  if (!ExistsFile(match_list_path)) {
+    std::cout << "Invalid matches file provided; trying spatial matching next"
+              << std::endl;
+    return false;
+  }
+  Database database(database_path);
+  size_t num_images_read = 0;
+  std::vector<std::string> match_image_pairs =
+      ReadTextFileLines(match_list_path);
+  std::unordered_set<std::string> match_images;
+
+  for (const std::string image_name_pair : match_image_pairs) {
+    std::vector<std::string> image_names = StringSplit(image_name_pair, " ");
+    match_images.insert(image_names[0]);
+    match_images.insert(image_names[1]);
+  }
+
+  for (const Image& image : database.ReadAllImages()) {
+    if (match_images.count(image.Name()) == 0) {
+      std::cout << "Image " << image.Name()
+                << " exists in the DB, but was not found in the match file; "
+                   "fixed image matching cannot be used, trying spatial "
+                   "matching next."
+                << std::endl;
+      return false;
+    }
+    num_images_read++;
+  }
+
+  if (match_images.size() == num_images_read) {
+    std::cout << "All images have valid pairings in the database; using "
+                 "fixed matching."
+              << std::endl;
+  } else {
+    std::cout << "WARN: Matching file contains more images than DB; "
+                 "additional images will be ignored."
+              << std::endl;
+  }
+  return true;
+}
+
 }  // namespace
 
 int RunFeatureExtractor(int argc, char** argv) {
@@ -144,6 +231,46 @@ int RunFeatureImporter(int argc, char** argv) {
   FeatureImporter feature_importer(reader_options, import_path);
   feature_importer.Start();
   feature_importer.Wait();
+
+  return EXIT_SUCCESS;
+}
+
+int RunAutoMatcher(int argc, char** argv) {
+  OptionManager options;
+  options.AddDatabaseOptions();
+  options.AddAutoMatchingOptions();
+  options.Parse(argc, argv);
+
+  std::unique_ptr<QApplication> app;
+  if (options.sift_matching->use_gpu && kUseOpenGL) {
+    app.reset(new QApplication(argc, argv));
+  }
+
+  std::shared_ptr<Thread> feature_matcher;
+  if (!options.image_pairs_matching->match_list_path.empty() &&
+      UseImagePairsMatcher(*options.database_path,
+                           options.image_pairs_matching->match_list_path)) {
+    feature_matcher.reset(new ImagePairsFeatureMatcher(
+        *options.image_pairs_matching, *options.sift_matching,
+        *options.database_path));
+  } else if (UseSpatialMatcher(*options.database_path,
+                               options.spatial_matching->max_distance,
+                               options.spatial_matching->is_gps)) {
+    feature_matcher.reset(new SpatialFeatureMatcher(*options.spatial_matching,
+                                                    *options.sift_matching,
+                                                    *options.database_path));
+  } else {
+    feature_matcher.reset(new ExhaustiveFeatureMatcher(
+        *options.exhaustive_matching, *options.sift_matching,
+        *options.database_path));
+  }
+
+  if (options.sift_matching->use_gpu && kUseOpenGL) {
+    RunThreadWithOpenGLContext(feature_matcher.get());
+  } else {
+    feature_matcher->Start();
+    feature_matcher->Wait();
+  }
 
   return EXIT_SUCCESS;
 }
