f2348f7a291c610f80821753dff084976ae4f3eb
Johannes Schönberger <joschonb@microsoft.com>
2022-01-25 21:42:06 +0100

    Fix camera size change under Windows

--- src/ui/model_viewer_widget.cc
+++ src/ui/model_viewer_widget.cc
@@ -628,14 +628,16 @@ void ModelViewerWidget::mouseMoveEvent(QMouseEvent* event) {
 }
 
 void ModelViewerWidget::wheelEvent(QWheelEvent* event) {
-  if (event->modifiers() & Qt::ControlModifier) {
-    ChangePointSize(event->angleDelta().y());
-  } else if (event->modifiers() & Qt::AltModifier) {
-    ChangeCameraSize(event->angleDelta().y());
-  } else if (event->modifiers() & Qt::ShiftModifier) {
-    ChangeNearPlane(event->angleDelta().y());
+  // We don't mind whether horizontal or vertical scroll.
+  const float delta = event->angleDelta().x() + event->angleDelta().y();
+  if (event->modifiers().testFlag(Qt::ControlModifier)) {
+    ChangePointSize(delta);
+  } else if (event->modifiers().testFlag(Qt::AltModifier)) {
+    ChangeCameraSize(delta);
+  } else if (event->modifiers().testFlag(Qt::ShiftModifier)) {
+    ChangeNearPlane(delta);
   } else {
-    ChangeFocusDistance(event->angleDelta().y());
+    ChangeFocusDistance(delta);
   }
   event->accept();
 }
