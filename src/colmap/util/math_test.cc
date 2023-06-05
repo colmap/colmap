82cf89ee5dc6140df66049d70ca831c2a79b726e
Johannes Schönberger <joschonb@microsoft.com>
2023-06-05 14:42:51 +0200

    Handle possible overflow in median function

--- src/colmap/util/math_test.cc
+++ src/colmap/util/math_test.cc
@@ -92,6 +92,8 @@ BOOST_AUTO_TEST_CASE(TestMedian) {
   BOOST_CHECK_EQUAL(Median<int>({-100, 1, 2, 3, 4}), 2);
   BOOST_CHECK_EQUAL(Median<int>({-1, -2, -3, -4}), -2.5);
   BOOST_CHECK_EQUAL(Median<int>({-1, -2, 3, 4}), 1);
+  // Test integer overflow scenario.
+  BOOST_CHECK_EQUAL(Median<uint8_t>({240, 248, 250, 255}), 249);
 }
 
 BOOST_AUTO_TEST_CASE(TestPercentile) {
