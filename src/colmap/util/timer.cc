2658b3588ccfbd0f6ef1897c994f9d189977f61b
Johannes Schönberger <joschonb@microsoft.com>
2023-07-05 14:14:12 +0200

    f

--- src/colmap/util/timer.cc
+++ src/colmap/util/timer.cc
@@ -34,8 +34,6 @@
 #include "colmap/util/logging.h"
 #include "colmap/util/misc.h"
 
-using namespace std::chrono;
-
 namespace colmap {
 
 Timer::Timer() : started_(false), paused_(false) {}
@@ -43,7 +41,7 @@ Timer::Timer() : started_(false), paused_(false) {}
 void Timer::Start() {
   started_ = true;
   paused_ = false;
-  start_time_ = high_resolution_clock::now();
+  start_time_ = std::chrono::high_resolution_clock::now();
 }
 
 void Timer::Restart() {
@@ -53,12 +51,12 @@ void Timer::Restart() {
 
 void Timer::Pause() {
   paused_ = true;
-  pause_time_ = high_resolution_clock::now();
+  pause_time_ = std::chrono::high_resolution_clock::now();
 }
 
 void Timer::Resume() {
   paused_ = false;
-  start_time_ += high_resolution_clock::now() - pause_time_;
+  start_time_ += std::chrono::high_resolution_clock::now() - pause_time_;
 }
 
 void Timer::Reset() {
@@ -71,10 +69,12 @@ double Timer::ElapsedMicroSeconds() const {
     return 0.0;
   }
   if (paused_) {
-    return duration_cast<microseconds>(pause_time_ - start_time_).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>(pause_time_ -
+                                                                 start_time_)
+        .count();
   } else {
-    return duration_cast<microseconds>(high_resolution_clock::now() -
-                                       start_time_)
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+               std::chrono::high_resolution_clock::now() - start_time_)
         .count();
   }
 }
