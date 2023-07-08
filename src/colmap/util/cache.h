6d82da919372fd53b96740ef36b5d21db536bc3f
Johannes Schönberger <joschonb@microsoft.com>
2023-07-08 19:52:53 +0300

    f

--- src/colmap/util/cache.h
+++ src/colmap/util/cache.h
@@ -217,7 +217,7 @@ size_t MemoryConstrainedLRUCache<key_t, value_t>::MaxNumBytes() const {
 
 template <typename key_t, typename value_t>
 void MemoryConstrainedLRUCache<key_t, value_t>::Set(const key_t& key,
-                                                    value_t&& value) {
+                                                    value_t value) {
   const size_t num_bytes = value.NumBytes();
   auto it = elems_map_.find(key);
   elems_list_.push_front(key_value_pair_t(key, std::move(value)));
--- src/colmap/util/cache_test.cc
+++ src/colmap/util/cache_test.cc
@@ -105,7 +105,7 @@ BOOST_AUTO_TEST_CASE(TestLRUCacheSet) {
   LRUCache<int, int> cache(5, [](const int key) { return -1; });
   BOOST_CHECK_EQUAL(cache.NumElems(), 0);
   for (int i = 0; i < 5; ++i) {
-    cache.Set(i, int(i));
+    cache.Set(i, i);
     BOOST_CHECK_EQUAL(cache.NumElems(), i + 1);
     BOOST_CHECK(cache.Exists(i));
   }
