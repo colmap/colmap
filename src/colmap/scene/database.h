6c98cd0aa7c752b1891bc73c19122c85df40c164
Johannes Schönberger <joschonb@microsoft.com>
2023-07-29 11:40:13 +0200

    Remove unnecessary checks in image pair conversion (#2074)

--- src/colmap/scene/database.h
+++ src/colmap/scene/database.h
@@ -361,8 +361,6 @@ class DatabaseTransaction {
 
 image_pair_t Database::ImagePairToPairId(const image_t image_id1,
                                          const image_t image_id2) {
-  CHECK_GE(image_id1, 0);
-  CHECK_GE(image_id2, 0);
   CHECK_LT(image_id1, kMaxNumImages);
   CHECK_LT(image_id2, kMaxNumImages);
   if (SwapImagePair(image_id1, image_id2)) {
@@ -377,8 +375,6 @@ void Database::PairIdToImagePair(const image_pair_t pair_id,
                                  image_t* image_id2) {
   *image_id2 = static_cast<image_t>(pair_id % kMaxNumImages);
   *image_id1 = static_cast<image_t>((pair_id - *image_id2) / kMaxNumImages);
-  CHECK_GE(*image_id1, 0);
-  CHECK_GE(*image_id2, 0);
   CHECK_LT(*image_id1, kMaxNumImages);
   CHECK_LT(*image_id2, kMaxNumImages);
 }
