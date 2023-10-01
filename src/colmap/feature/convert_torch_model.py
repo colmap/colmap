9b5485a9e6e959f92cb1589a95278552ff901250
Johannes Schönberger <joschonb@microsoft.com>
2023-10-01 14:34:39 +0200

    d

--- src/colmap/feature/convert_torch_model.py
+++ src/colmap/feature/convert_torch_model.py
@@ -1,11 +1,8 @@
 import torch
-
 import kornia
-from kornia.feature.laf import (
-    laf_from_center_scale_ori, extract_patches_from_pyramid)
 
 def disk(image):
-    model = kornia.feature.DISK()
+    model = kornia.feature.DISK.from_pretrained("depth")
     features = model.forward(image[None, ...])
     return (features[0].keypoints, features[0].descriptors)
 
@@ -15,9 +12,5 @@ def sift(image):
     return (features[0], features[2])
 
 inputs = torch.rand([3, 256, 256])
-a = disk(inputs)
-b = disk(inputs)
-assert torch.all(a[0] == b[0])
-
-# traced_script_module = torch.jit.trace(disk, inputs)
-# traced_script_module.save("disk.pt")
+traced_script_module = torch.jit.trace(disk, inputs)
+traced_script_module.save("disk.pt")
