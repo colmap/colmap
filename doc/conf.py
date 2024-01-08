2d75c65786f1183ea8049d3274a395eaf6c01751
Johannes Schönberger (from Dev Box) <joschonb@microsoft.com>
2024-01-08 17:19:57 +0100

    Patch release 3.9.1

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,7 @@ copyright = u"2023, Johannes L. Schoenberger"
 # built documents.
 #
 # The short X.Y version.
-version = "3.9"
+version = "3.9.1"
 # The full version, including alpha/beta/rc tags.
 release = version
 
