265130ff669097bb772c10d7e64a69f51f746148
B1ueber2y <b1ueber2y@gmail.com>
2024-03-22 15:30:44 +0100

    fix format.

--- src/pycolmap/scene/database_cache.cc
+++ src/pycolmap/scene/database_cache.cc
@@ -24,6 +24,7 @@ void BindDatabaseCache(py::module& m) {
       .def("exists_image", &DatabaseCache::ExistsImage, "image_id"_a)
       .def_property_readonly("cameras", &DatabaseCache::Cameras)
       .def_property_readonly("images", &DatabaseCache::Images)
-      .def_property_readonly("correspondence_graph", &DatabaseCache::CorrespondenceGraph)
+      .def_property_readonly("correspondence_graph",
+                             &DatabaseCache::CorrespondenceGraph)
       .def("find_image_with_name", &DatabaseCache::FindImageWithName);
 }
