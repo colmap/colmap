8f119160b90ce93b3bd92bbbf644a6c7a68e73af
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2024-03-23 18:09:36 +0100

    Remove duplicated example code

--- pycolmap/custom_incremental_mapping.py
+++ pycolmap/custom_incremental_mapping.py
@@ -265,7 +265,7 @@ def main_incremental_mapper(controller):
     timer.print_minutes()
 
 
-def incremental_mapping(
+def main(
     database_path,
     image_path,
     output_path,
@@ -278,7 +278,7 @@ def incremental_mapping(
         logging.fatal(f"Image path does not exist: {image_path}")
     output_path.mkdir(exist_ok=True, parents=True)
     reconstruction_manager = pycolmap.ReconstructionManager()
-    if (input_path is not None) and input_path != "":
+    if input_path is not None and input_path != "":
         reconstruction_manager.read(input_path)
     mapper = pycolmap.IncrementalMapperController(
         options, image_path, database_path, reconstruction_manager
@@ -307,60 +307,3 @@ def incremental_mapping(
     for i in range(reconstruction_manager.size()):
         reconstructions[i] = reconstruction_manager.get(i)
     return reconstructions
-
-
-def incremental_mapping_cpp(database_path, image_path, sfm_path):
-    num_images = pycolmap.Database(database_path).num_images
-    with enlighten.Manager() as manager:
-        with manager.counter(
-            total=num_images, desc="Images registered:"
-        ) as pbar:
-            pbar.update(0, force=True)
-            recs = pycolmap.incremental_mapping(
-                database_path,
-                image_path,
-                sfm_path,
-                initial_image_pair_callback=lambda: pbar.update(2),
-                next_image_callback=lambda: pbar.update(1),
-            )
-    return recs
-
-
-def run():
-    output_path = Path("example/")
-    image_path = output_path / "Fountain/images"
-    database_path = output_path / "database.db"
-    sfm_path = output_path / "sfm"
-
-    output_path.mkdir(exist_ok=True)
-    logging.set_log_destination(
-        logging.INFO, output_path / "INFO.log."
-    )  # + time
-
-    data_url = "https://cvg-data.inf.ethz.ch/local-feature-evaluation-schoenberger2017/Strecha-Fountain.zip"
-    if not image_path.exists():
-        logging.info("Downloading the data.")
-        zip_path = output_path / "data.zip"
-        urllib.request.urlretrieve(data_url, zip_path)
-        with zipfile.ZipFile(zip_path, "r") as fid:
-            fid.extractall(output_path)
-        logging.info(f"Data extracted to {output_path}.")
-
-    if database_path.exists():
-        database_path.unlink()
-    pycolmap.set_random_seed(0)
-    pycolmap.extract_features(database_path, image_path)
-    pycolmap.match_exhaustive(database_path)
-
-    if sfm_path.exists():
-        shutil.rmtree(sfm_path)
-    sfm_path.mkdir(exist_ok=True)
-
-    recs = incremental_mapping(database_path, image_path, sfm_path)
-
-    for idx, rec in recs.items():
-        logging.info(f"#{idx} {rec.summary()}")
-
-
-if __name__ == "__main__":
-    run()
