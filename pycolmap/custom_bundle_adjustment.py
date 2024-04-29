e71d297dc098f95e6458ec36189031204040b990
B1ueber2y <b1ueber2y@gmail.com>
2024-04-29 12:13:04 +0200

    minor. add pyceres comments to the head of BA python file

--- pycolmap/custom_bundle_adjustment.py
+++ pycolmap/custom_bundle_adjustment.py
@@ -1,6 +1,7 @@
 """
 Python reimplementation of the bundle adjustment for the incremental mapper of C++ with equivalent logic.
 As a result, one can add customized residuals on top of the exposed ceres problem from conventional bundle adjustment.
+pyceres is needed as a dependency for this file.
 """
 
 import pyceres
