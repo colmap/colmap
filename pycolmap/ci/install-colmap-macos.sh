3990b73a75a27a62a887ff18b337087c89f67801
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2024-05-27 14:28:23 +0200

    Disable LSD when building pycolmap wheels

--- pycolmap/ci/install-colmap-macos.sh
+++ pycolmap/ci/install-colmap-macos.sh
@@ -23,9 +23,10 @@ cd ${CURRDIR}
 mkdir build && cd build
 export ARCHFLAGS="-arch ${CIBW_ARCHS_MACOS}"
 cmake .. -GNinja \
-    -DGUI_ENABLED=OFF \
     -DCUDA_ENABLED=OFF \
+    -DGUI_ENABLED=OFF \
     -DCGAL_ENABLED=OFF \
+    -DLSD_ENABLED=OFF \
     -DCMAKE_BUILD_TYPE=Release \
     -DCCACHE_ENABLED=ON \
     -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TOOLCHAIN_FILE}" \
