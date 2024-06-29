6a2813f4e9036e8bfe57d7f5b7f0dee2daffe54a
B1ueber2y <b1ueber2y@gmail.com>
2024-06-29 23:09:33 +0200

    fix binding for sim3d.scale.

--- src/pycolmap/geometry/bindings.cc
+++ src/pycolmap/geometry/bindings.cc
@@ -114,12 +114,14 @@ void BindGeometry(py::module& m) {
       .def(
           py::init<double, const Eigen::Quaterniond&, const Eigen::Vector3d&>())
       .def(py::init(&Sim3d::FromMatrix))
-      .def_readwrite("scale", &Sim3d::scale)
+      .def_property(
+          "scale",
+          [](Sim3d& self) {
+            return py::array({}, {}, &self.scale, py::cast(self));
+          },
+          [](Sim3d& self, double scale) { self.scale = scale; })
       .def_readwrite("rotation", &Sim3d::rotation)
       .def_readwrite("translation", &Sim3d::translation)
-      .def_property_readonly(
-          "scale_ptr",  // for optimization
-          [](Sim3d& self) { return py::array(1, &self.scale); })
       .def("matrix", &Sim3d::ToMatrix)
       .def(py::self * Sim3d())
       .def(py::self * Eigen::Vector3d())
