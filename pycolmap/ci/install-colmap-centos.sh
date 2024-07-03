2141a0b9034da8fafe127c094b587c3c180bdb93
Johannes Schönberger <joschonb@microsoft.com>
2024-07-03 13:53:59 +0000

    d

--- pycolmap/ci/install-colmap-centos.sh
+++ pycolmap/ci/install-colmap-centos.sh
@@ -5,16 +5,8 @@ CURRDIR=$(pwd)
 
 yum install -y gcc gcc-c++ ninja-build curl zip unzip tar
 
-# ccache shipped by CentOS is too old so we download and cache it.
-COMPILER_TOOLS_DIR="${CONTAINER_COMPILER_CACHE_DIR}/bin"
-mkdir -p ${COMPILER_TOOLS_DIR}
-if [ ! -f "${COMPILER_TOOLS_DIR}/ccache" ]; then
-    FILE="ccache-4.9-linux-x86_64"
-    curl -sSLO https://github.com/ccache/ccache/releases/download/v4.9/${FILE}.tar.xz
-    tar -xf ${FILE}.tar.xz
-    cp ${FILE}/ccache ${COMPILER_TOOLS_DIR}
-fi
-export PATH="${COMPILER_TOOLS_DIR}:${PATH}"
+# TODO: REMOVE
+ninja --version
 
 # Setup vcpkg
 git clone https://github.com/microsoft/vcpkg ${VCPKG_INSTALLATION_ROOT}
