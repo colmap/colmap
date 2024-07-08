d9a2acfe14d5df191fda283149ba4155f1af4572
Johannes Schönberger <joschonb@microsoft.com>
2024-07-08 11:26:41 +0000

    d

--- pycolmap/ci/install-colmap-macos.sh
+++ pycolmap/ci/install-colmap-macos.sh
@@ -8,6 +8,11 @@ find /usr/local/bin -lname '*/Library/Frameworks/Python.framework/*' -delete
 brew update
 brew install git cmake ninja llvm gfortran ccache
 
+export PATH="/usr/local/opt/llvm/bin:$PATH"
+export CPPFLAGS="-I/usr/local/opt/llvm/include"
+export LDFLAGS="-L/usr/local/opt/llvm/lib/c++ -Wl,-rpath,/usr/local/opt/llvm/lib/c++"
+export ARCHFLAGS="-arch ${CIBW_ARCHS_MACOS}"
+
 # When building lapack-reference, vcpkg/cmake looks for gfortran.
 ln -s $(which gfortran-14) "$(dirname $(which gfortran-14))/gfortran"
 
@@ -21,7 +26,6 @@ git checkout ${VCPKG_COMMIT_ID}
 # Build COLMAP
 cd ${CURRDIR}
 mkdir build && cd build
-export ARCHFLAGS="-arch ${CIBW_ARCHS_MACOS}"
 cmake .. -GNinja \
     -DCUDA_ENABLED=OFF \
     -DGUI_ENABLED=OFF \
@@ -29,6 +33,10 @@ cmake .. -GNinja \
     -DLSD_ENABLED=OFF \
     -DCCACHE_ENABLED=ON \
     -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_MAKE_PROGRAM=/usr/local/bin/ninja \
+    -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang\
+    -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++ \
+    -DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran \
     -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TOOLCHAIN_FILE}" \
     -DVCPKG_TARGET_TRIPLET=${VCPKG_TARGET_TRIPLET} \
     -DCMAKE_OSX_ARCHITECTURES=${CMAKE_OSX_ARCHITECTURES} \
