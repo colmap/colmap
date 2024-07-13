5f594344239544406d3e91e874cad5f3e0763a1c
Johannes Schönberger <joschonb@microsoft.com>
2024-07-13 10:40:05 +0000

    d

--- pycolmap/ci/install-colmap-macos.sh
+++ pycolmap/ci/install-colmap-macos.sh
@@ -8,9 +8,10 @@ find /usr/local/bin -lname '*/Library/Frameworks/Python.framework/*' -delete
 brew update
 brew install git cmake ninja llvm gfortran ccache
 
-export PATH="/usr/local/opt/llvm/bin:$PATH"
-export CPPFLAGS="-I/usr/local/opt/llvm/include"
-export LDFLAGS="-L/usr/local/opt/llvm/lib/c++ -Wl,-rpath,/usr/local/opt/llvm/lib/c++"
+llvm_prefix="$(brew --prefix llvm)"
+export PATH="$llvm_prefix/bin:$PATH"
+export CPPFLAGS="-I$llvm_prefix/include"
+export LDFLAGS="-L$llvm_prefix/lib/c++ -Wl,-rpath,$llvm_prefix/lib/c++"
 export ARCHFLAGS="-arch ${CIBW_ARCHS_MACOS}"
 export MACOSX_DEPLOYMENT_TARGET="${CMAKE_OSX_DEPLOYMENT_TARGET}"
 
@@ -27,17 +28,17 @@ git checkout ${VCPKG_COMMIT_ID}
 # Build COLMAP
 cd ${CURRDIR}
 mkdir build && cd build
-cmake .. -GNinja \
+$(brew --prefix cmake)/bin/cmake .. -GNinja \
     -DCUDA_ENABLED=OFF \
     -DGUI_ENABLED=OFF \
     -DCGAL_ENABLED=OFF \
     -DLSD_ENABLED=OFF \
     -DCCACHE_ENABLED=ON \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_MAKE_PROGRAM=/usr/local/bin/ninja \
-    -DCMAKE_C_COMPILER=/usr/local/opt/llvm/bin/clang\
-    -DCMAKE_CXX_COMPILER=/usr/local/opt/llvm/bin/clang++ \
-    -DCMAKE_Fortran_COMPILER=/usr/local/bin/gfortran \
+    -DCMAKE_MAKE_PROGRAM="$(brew --prefix ninja)/bin/ninja" \
+    -DCMAKE_C_COMPILER="$llvm_prefix/bin/clang" \
+    -DCMAKE_CXX_COMPILER="$llvm_prefix/bin/clang++" \
+    -DCMAKE_Fortran_COMPILER="/usr/local/bin/gfortran" \
     -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TOOLCHAIN_FILE}" \
     -DVCPKG_TARGET_TRIPLET="${VCPKG_TARGET_TRIPLET}" \
     -DCMAKE_OSX_ARCHITECTURES="${CMAKE_OSX_ARCHITECTURES}" \
