9af30e1bab5564df0818b02df40b65803f939405
B1ueber2y <b1ueber2y@gmail.com>
2024-07-28 01:20:51 +0200

    minor.

--- src/colmap/estimators/cost_functions.h
+++ src/colmap/estimators/cost_functions.h
@@ -455,7 +455,8 @@ struct MetricRelativePoseErrorCostFunction {
 
 // Cost function for aligning one 3D point with a reference 3D point with
 // covariance. Convention is similar to colmap::Sim3d
-// r = scale * R * point + t - ref_point
+// residual = scale_b_from_a * R_b_from_a * point_in_a + t_b_from_a -
+// ref_point_in_b
 struct Point3dAlignmentCostFunction {
  public:
   Point3dAlignmentCostFunction(const Eigen::Vector3d& ref_point,
