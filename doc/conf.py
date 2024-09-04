f0162aa4eb4224deb29b0dd02ff4998666dc355a
Paul-Edouard Sarlin <15985472+sarlinpe@users.noreply.github.com>
2024-09-04 16:34:27 +0200

    PyCOLMAP API documentation (#2749)
    * Cleanup docstrings
    * Pin Sphinx versions
    * Add pycolmap API doc

--- doc/conf.py
+++ doc/conf.py
@@ -30,6 +30,7 @@ import os
 # ones.
 extensions = [
     "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -46,7 +47,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"COLMAP"
-copyright = u"2023, Johannes L. Schoenberger"
+copyright = u"2024, Johannes L. Schoenberger"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -129,7 +130,8 @@ html_theme_path = ["_themes"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
