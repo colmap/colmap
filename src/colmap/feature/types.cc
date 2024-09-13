1aad9bbb680c51f6808584f6e01f2edd53fad701
B1ueber2y <b1ueber2y@gmail.com>
2024-09-13 23:59:23 +0200

    minor. update check condition.

--- src/colmap/feature/types.cc
+++ src/colmap/feature/types.cc
@@ -94,9 +94,10 @@ FeatureKeypoint::FeatureKeypoint(const float x_,
 void FeatureKeypoint::InitializeCovariance(const Eigen::Matrix2d& covar) {
   THROW_CHECK_EQ(covar(0, 1), covar(1, 0))
       << "Covariance matrix needs to be symmetric.";
-  THROW_CHECK_GT(covar(0, 0), 0);
-  THROW_CHECK_GT(covar(1, 1), 0);
-  THROW_CHECK_GT(covar.determinant(), 0);
+  THROW_CHECK_GE(covar(0, 0), 0);
+  THROW_CHECK_GE(covar(1, 1), 0);
+  THROW_CHECK_GE(covar.determinant(), 0)
+      << "Covariance matrix needs to be PSD.";
   s11 = covar(0, 0);
   s12 = covar(0, 1);
   s22 = covar(1, 1);
