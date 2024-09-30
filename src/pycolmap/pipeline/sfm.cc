81232d0dfc62265edfe458b727deb230fa6f93bb
Johannes Schönberger <jsch@demuc.de>
2024-09-30 16:01:38 +0200

    d

--- src/pycolmap/pipeline/sfm.cc
+++ src/pycolmap/pipeline/sfm.cc
@@ -4,6 +4,7 @@
 #include "colmap/controllers/incremental_mapper.h"
 #include "colmap/scene/reconstruction.h"
 #include "colmap/sensor/models.h"
+#include "colmap/util/file.h"
 #include "colmap/util/misc.h"
 
 #include "pycolmap/helpers.h"
