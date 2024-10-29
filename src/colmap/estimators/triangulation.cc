7e27e2fa16effe595d84b079b7e18d29d5293845
Johannes Schönberger <jsch@demuc.de>
2024-10-29 09:10:27 +0100

    Avoid unnecessary copy of RANSAC inlier masks

--- src/colmap/estimators/triangulation.cc
+++ src/colmap/estimators/triangulation.cc
@@ -176,12 +176,12 @@ bool EstimateTriangulation(const EstimateTriangulationOptions& options,
   ransac.estimator.SetResidualType(options.residual_type);
   ransac.local_estimator.SetMinTriAngle(options.min_tri_angle);
   ransac.local_estimator.SetResidualType(options.residual_type);
-  const auto report = ransac.Estimate(point_data, pose_data);
+  auto report = ransac.Estimate(point_data, pose_data);
   if (!report.success) {
     return false;
   }
 
-  *inlier_mask = report.inlier_mask;
+  *inlier_mask = std::move(report.inlier_mask);
   *xyz = report.model;
 
   return report.success;
