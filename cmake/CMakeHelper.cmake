f959b5133b513d08404d73dec6b9700213aa19c5
Johannes Schönberger <jsch@demuc.de>
2024-10-31 14:31:02 +0100

    Fix duplicate library warnings in linking stage

--- cmake/CMakeHelper.cmake
+++ cmake/CMakeHelper.cmake
@@ -133,8 +133,6 @@ macro(COLMAP_ADD_TEST)
         endif()
         target_link_libraries(${COLMAP_ADD_TEST_NAME}
             ${COLMAP_ADD_TEST_LINK_LIBS}
-            GTest::gtest
-            GTest::gmock
             GTest::gmock_main)
         add_test("${FOLDER_NAME}/${COLMAP_ADD_TEST_NAME}" ${COLMAP_ADD_TEST_NAME})
         if(IS_MSVC)
