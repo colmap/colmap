12b0e4ef30d4a9ba5a27cd9f598fd322cd94e391
B1ueber2y <b1ueber2y@gmail.com>
2024-11-06 20:32:21 +0100

    formatting.

--- src/colmap/estimators/problem_partitioner.cc
+++ src/colmap/estimators/problem_partitioner.cc
@@ -44,8 +44,7 @@ ProblemPartitioner::BipartiteGraph::BipartiteGraph(ceres::Problem* problem) {
                                                 &param_blocks);
 
     for (auto& param_block : param_blocks) {
-      if (problem->IsParameterBlockConstant(param_block))
-        continue;
+      if (problem->IsParameterBlockConstant(param_block)) continue;
       AddEdge(param_block, residual_block_id);
     }
   }
