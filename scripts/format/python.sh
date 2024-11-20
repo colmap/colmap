1dd720ac415cd43eeeda43b414b6371784b77270
B1ueber2y <b1ueber2y@gmail.com>
2024-11-20 12:16:44 +0100

    fix

--- scripts/format/python.sh
+++ scripts/format/python.sh
@@ -23,4 +23,4 @@ echo "Formatting ${num_files} files"
 
 # shellcheck disable=SC2086
 ruff format --config ${root_folder}/ruff.toml ${all_files}
-ruff check --fix --config ${root_folder}/ruff.toml ${all_files}
+ruff check --config ${root_folder}/ruff.toml ${all_files}
