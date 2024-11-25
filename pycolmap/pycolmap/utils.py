colmap/colmap

commit 4470a3ea3dc7162a0711101dfc96727482df397b

Cleaner import of C++ symbols (#2933)

pycolmap/pycolmap/utils.py
@@ -1,0 +1,18 @@
+from types import ModuleType
+from typing import Any, MutableSequence
+
+
+def import_module_symbols(
+    dst_vars: dict[str, Any],
+    src_module: ModuleType,
+    exclude: set[str] | None = None,
+) -> MutableSequence[str]:
+    symbols = {}
+    for n, s in vars(src_module).items():
+        if n.startswith("_"):
+            continue
+        if exclude is not None and n in exclude:
+            continue
+        symbols[n] = s
+    dst_vars.update(symbols)
+    return list(symbols)

pycolmap/pycolmap/__init__.py
@@ -1,14 +1,27 @@
-from ._core import *  # noqa F403
+import textwrap
+from typing import TYPE_CHECKING
 
-# This is unfortunately needed otherwise the C++ modules will shadow the
-# equivalent python modules, which already import them. We can remove this
-# once pycolmap._core has few declarations such that they can be explicitly
-# imported.
-del cost_functions, manifold  # noqa F821
+from .utils import import_module_symbols
 
-from . import _core  # noqa E402
+try:
+    from . import _core
+except ImportError as e:
+    raise RuntimeError(
+        textwrap.dedent("""
+        Cannot import the C++ backend pycolmap._core.
+        Make sure that you successfully install the package with
+          $ python -m pip install pycolmap/
+        """)
+    ) from e
 
-__all__ = [n for n in _core.__dict__ if not n.startswith("_")]
+# Type checkers cannot deal with dynamic manipulation of globals.
+# Instead, we use the same workaround as PyTorch.
+if TYPE_CHECKING:
+    from ._core import *  # noqa F403
+
+__all__ = import_module_symbols(
+    globals(), _core, exclude={"cost_functions", "manifold"}
+)
 __all__.extend(["__version__", "__ceres_version__"])
 
 __version__ = _core.__version__

pycolmap/pycolmap/cost_functions/__init__.py
@@ -1,4 +1,10 @@
-from .._core.cost_functions import *  # noqa
-from .._core import cost_functions as _core
+from typing import TYPE_CHECKING
 
-__all__ = [n for n in _core.__dict__ if not n.startswith("_")]
+from .. import _core
+from ..utils import import_module_symbols
+
+if TYPE_CHECKING:
+    from .._core.cost_functions import *  # noqa
+
+__all__ = import_module_symbols(globals(), _core.cost_functions)
+del _core

pycolmap/pycolmap/manifold/__init__.py
@@ -1,4 +1,10 @@
-from .._core.manifold import *  # noqa
-from .._core import manifold as _core
+from typing import TYPE_CHECKING
 
-__all__ = [n for n in _core.__dict__ if not n.startswith("_")]
+from .. import _core
+from ..utils import import_module_symbols
+
+if TYPE_CHECKING:
+    from .._core.manifold import *  # noqa
+
+__all__ = import_module_symbols(globals(), _core.manifold)
+del _core

pycolmap/pyproject.toml
@@ -2,7 +2,7 @@
 requires = [
     "scikit-build-core>=0.3.3",
     "pybind11==2.13.0",
-    "pybind11_stubgen @ git+https://github.com/sarlinpe/pybind11-stubgen@sarlinpe/fix-2024-08",
+    "pybind11_stubgen @ git+https://github.com/sarlinpe/pybind11-stubgen@sarlinpe/fix-2024-11",
     "numpy",
     "ruff==0.6.7",
     "clang-format==19.1.0",

commit 0565f609567eb127e5fa7220d5dc21a39d14a198

Fix pycolmap breakage for Python 3.8 (#2941)
Introduced by https://github.com/colmap/colmap/pull/2933

.github/workflows/build-pycolmap.yml
@@ -29,7 +29,7 @@ jobs:
       CCACHE_BASEDIR: ${{ github.workspace }}
       MACOSX_DEPLOYMENT_TARGET: 10.15
       # For faster builds in PRs, skip all but the latest Python versions.
-      PULL_REQUEST_CIBW_BUILD: cp312-{macosx,manylinux,win}*
+      PULL_REQUEST_CIBW_BUILD: cp38-{macosx,manylinux,win}*
     steps:
       - uses: actions/checkout@v4
       - uses: actions/cache@v4

pycolmap/pycolmap/utils.py
@@ -1,11 +1,11 @@
 from types import ModuleType
-from typing import Any, MutableSequence
+from typing import Any, Dict, MutableSequence, Optional, Set
 
 
 def import_module_symbols(
-    dst_vars: dict[str, Any],
+    dst_vars: Dict[str, Any],
     src_module: ModuleType,
-    exclude: set[str] | None = None,
+    exclude: Optional[Set[str]] = None,
 ) -> MutableSequence[str]:
     symbols = {}
     for n, s in vars(src_module).items():