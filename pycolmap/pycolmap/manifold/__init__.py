f9b21de56af5a2c5625a462b725d2a12fb7f886e
sarlinpe <paul.edouard.sarlin@gmail.com>
2024-11-25 14:12:22 +0100

    Apply fix to submodules

--- pycolmap/pycolmap/manifold/__init__.py
+++ pycolmap/pycolmap/manifold/__init__.py
@@ -1,5 +1,10 @@
+from typing import TYPE_CHECKING
+
 from .. import _core
 from ..utils import import_module_symbols
+
+if TYPE_CHECKING:
+    from .._core.manifold import *  # noqa
 
 __all__ = import_module_symbols(globals(), _core.manifold)
 del _core
