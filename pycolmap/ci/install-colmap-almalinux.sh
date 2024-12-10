e062987126c9f2fc9cb6ea188b20eae935600425
Johannes Schönberger <jsch@demuc.de>
2024-12-10 10:54:21 +0100

    d

--- pycolmap/ci/install-colmap-almalinux.sh
+++ pycolmap/ci/install-colmap-almalinux.sh
@@ -11,6 +11,7 @@ yum install -y \
     gcc-toolset-12-gcc \
     gcc-toolset-12-gcc-c++ \
     gcc-toolset-12-gcc-gfortran \
+    kernel-devel-$(uname -r) \
     scl-utils \
     git \
     cmake3 \
