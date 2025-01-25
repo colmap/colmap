d45dacc621163427fcc60f65b486332518964bde
Johannes Schönberger <jsch@demuc.de>
2025-01-25 17:34:43 +0100

    Remove year from copyright (#3124)
    Prevent yearly updates (that we forgot last year) by removing
    unnecessary year in the copyright notice.

--- scripts/shell/profile_binary.sh
+++ scripts/shell/profile_binary.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2023, ETH Zurich and UNC Chapel Hill.
+# Copyright (c), ETH Zurich and UNC Chapel Hill.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
