93c1168d3aac2cee69f30ca1c304df2926023322
Johannes Schönberger <jsch@demuc.de>
2025-02-10 07:49:16 +0100

    d

--- src/colmap/geometry/essential_matrix_test.cc
+++ src/colmap/geometry/essential_matrix_test.cc
@@ -166,15 +166,15 @@ TEST(FundamentalFromEssentialMatrix, Nominal) {
 }
 
 TEST(EssentialFromFundamentalMatrix, Nominal) {
-  const Eigen::Matrix3d F = EssentialMatrixFromPose(
+  const Eigen::Matrix3d E = EssentialMatrixFromPose(
       Rigid3d(Eigen::Quaterniond::UnitRandom(), Eigen::Vector3d::Random()));
   const Eigen::Matrix3d K1 =
       (Eigen::Matrix3d() << 2, 0, 1, 0, 3, 2, 0, 0, 1).finished();
   const Eigen::Matrix3d K2 =
       (Eigen::Matrix3d() << 3, 0, 2, 0, 4, 1, 0, 0, 1).finished();
-  const Eigen::Matrix3d E = EssentialFromFundamentalMatrix(K2, F, K1);
-  EXPECT_THAT(FundamentalFromEssentialMatrix(K2, E, K1),
-              EigenMatrixNear(F, 1e-6));
+  const Eigen::Matrix3d F = FundamentalFromEssentialMatrix(K2, E, K1);
+  EXPECT_THAT(EssentialFromFundamentalMatrix(K2, F, K1),
+              EigenMatrixNear(E, 1e-6));
 }
 
 }  // namespace
