79170ecf3d3ba0c30b0c2732b13649210d00400b
StonerLing <stoner.ling@qq.com>
2025-02-11 17:55:20 +0800

    Add class representing a hierarchical coordinate system context

--- src/colmap/geometry/pose_prior.h
+++ src/colmap/geometry/pose_prior.h
@@ -29,17 +29,26 @@
 
 #pragma once
 
+#include "colmap/geometry/rigid3.h"
 #include "colmap/util/eigen_alignment.h"
 #include "colmap/util/enum_utils.h"
 #include "colmap/util/types.h"
 
+#include <optional>
 #include <ostream>
 
 #include <Eigen/Core>
 
 namespace colmap {
 
-struct PosePrior {
+// Represents a hierarchical coordinate system context.
+//
+// This structure models a coordinate system hierarchy, where each coordinate
+// system is defined relative to its parent frame. It allows defining and
+// managing relationships between global, regional, and local coordinate
+// systems, such as WGS84, ECEF, ENU, UTM, body frame and model frame.
+// e.g. WGS84/GRS80 -> UTM/ENU -> OBJECT_1 -> OBJECT_2, MODEL_1 -> MODEL_2
+struct CoordinateSystemContext {
  public:
   MAKE_ENUM_CLASS(CoordinateSystem,
                   -1,
@@ -48,11 +57,210 @@ struct PosePrior {
                   ECEF,       // = 1
                   GRS80,      // = 2
                   ENU,        // = 3
-                  UTM         // = 4
+                  UTM,        // = 4
+                  OBJECT,     // = 5, body frame
+                  MODEL       // = 6, non-metric frame
   );
 
-  inline bool IsCartesian(CoordinateSystem system) const;
+  MAKE_ENUM(CoordinateSystemLevel,
+            -1,
+            UNDEFINED,
+            // 0, Global geocentric frame, e.g. WGS84/GRS80/ECEF
+            GLOBAL,
+            // 1, Regional geodetic frame, based on global frame, e.g. ENU/UTM
+            REGIONAL,
+            // 2, Local object frame, based on regional frame, e.g. body frame
+            LOCAL,
+            // 3, Virtual model frame, non-metric and used for simulation
+            VIRTUAL);
 
+  // Directly represents the pose of a frame, including orientation and position
+  // in parent frame. Not use Rigid3d since WGS84/GRS80 is spherical.
+  struct FramePose {
+    Eigen::Quaterniond orientation = Eigen::Quaterniond::Identity();
+    Eigen::Vector3d position = Eigen::Vector3d::Zero();
+
+    // Returns the corresponding Rigid3d transformation from parent.
+    // Assumes the pose position is in Cartesian coordinates.
+    Rigid3d ToRigid3d() const;
+
+    // Returns a FramePose representing the identity transformation:
+    // Position (0, 0, 0) and rotation as the identity matrix.
+    static FramePose Identity();
+  };
+
+  // Represents a node in a hierarchical coordinate system.
+  // Each FrameNode has a parent frame (if any), and an pose describing the
+  // position and orientation of the current coordinate system relative to its
+  // parent frame.
+  struct FrameNode {
+    CoordinateSystem coordinate_system = CoordinateSystem::UNDEFINED;
+    // Pose of current coordinate system in parent frame, defines the position
+    // and orientaion in parent frame, it's null for global and virtual
+    // coordinate system.
+    FramePose frame_pose = FramePose::Identity();
+
+    // Parent frame for hierarchical coordinate systems, it's null for global
+    // and virtual coordinate system.
+    std::unique_ptr<FrameNode> parent_frame = nullptr;
+
+    // Unique identifier for frame, mainly used for object/model frame.
+    std::uint32_t frame_id = 0;
+
+    explicit FrameNode(CoordinateSystem system,
+                       const FramePose& pose = FramePose::Identity(),
+                       FrameNode* parent = nullptr,
+                       std::uint32_t id = 0);
+    FrameNode(CoordinateSystem system, std::uint32_t id);
+    FrameNode(const FrameNode& other);
+
+    CoordinateSystemLevel Level() const;
+
+    inline bool IsCartesian() const;
+    inline bool IsMetric() const;
+
+    Eigen::Vector3d ConvertFromParent(
+        const Eigen::Vector3d& coordinate_in_parent);
+    Eigen::Vector3d ConvertToParent(
+        const Eigen::Vector3d& coordinate_in_current);
+
+    // Two frame node equals if they have the same coordinate system name and
+    // same id.
+    inline bool operator==(const FrameNode& other) const;
+    inline bool operator!=(const FrameNode& other) const;
+  };
+
+  CoordinateSystemContext() {};
+  CoordinateSystemContext(const CoordinateSystemContext& other);
+
+  // This constructor initializes a coordinate system context from a list of
+  // frames.
+  //
+  // e.g. Construct a context: object <- utm <- WGS84
+  // ```cpp
+  // std::vector<std::pair<FrameNode, FramePose> > frame_chain;
+  // frame_chain.emplace_back(FrameNode{CoordinateSystem::OBJECT},
+  //                          FramePose::Identity());
+  // frame_chain.emplace_back(FrameNode{CoordinateSystem::UTM},
+  //                          object_in_utm);
+  // frame_chain.emplace_back(FrameNode{CoordinateSystem::WGS84},
+  //                          utm_in_wgs84);
+  // CoordinateSystemContext context_1(frame_chain);
+  // ```
+  explicit CoordinateSystemContext(
+      const std::vector<std::pair<FrameNode, FramePose> >& frame_chain,
+      bool reverse = false);
+
+  ~CoordinateSystemContext();
+
+  // Appends a new parent frame node to the current coordinate system hierarchy
+  void Append(const FrameNode& frame, const FramePose& current_base_in_appened);
+  // Prepends a new parent frame node to the current coordinate system
+  // hierarchy.
+  void Prepend(const FrameNode& frame, const FramePose& prepended_in_current);
+
+  // Retrieves the last frame in the hierarchical coordinate system.
+  std::optional<FrameNode> MaybeBaseFrame() const;
+  // Retrieves the child of base frame (the second last frame in the
+  // hierarchical coordinate system).
+  std::optional<FrameNode> MaybePenultimateFrame() const;
+
+  // Serializes the entire coordinate system hierarchy into an Eigen matrix.
+  // The resulting matrix has the following structure:
+  // - Each row: Corresponds to one frame node in the hierarchy.
+  // - Column 1: Coordinate system enum.
+  // - Columns 2 to 4: Position (x, y, z).
+  // - Columns 5 to 8: Orientation (x, y, z, w).
+  // - Column 9: Coordinate frame id.
+  Eigen::MatrixXd ToMatrix() const;
+  // Serializes the entire coordinate system hierarchy into a string, every 9
+  // elements separated by ',' represent one frame node (one row of the
+  // serialized matrix).
+  std::string ToString() const;
+
+  Eigen::Vector3d ConvertFromSpecifiedParent(
+      const Eigen::Vector3d& coordinate_in_specified,
+      const FrameNode& specified_frame) const;
+  Eigen::Vector3d ConvertToSpecifiedParent(
+      const Eigen::Vector3d& coordinate_in_current,
+      const FrameNode& specified_frame) const;
+
+  Eigen::Vector3d ConvertFromBase(
+      const Eigen::Vector3d& coordinate_in_base) const;
+  Eigen::Vector3d ConvertToBase(
+      const Eigen::Vector3d& coordinate_in_current) const;
+
+  inline std::optional<CoordinateSystemLevel> MaybeCurrentLevel() const;
+  inline std::optional<CoordinateSystem> MaybeCurrentCoordinateSystem() const;
+
+  std::optional<Eigen::Vector3d> MaybeCurrentPosition() const;
+  std::optional<Eigen::Quaterniond> MaybeCurrentOrientation() const;
+
+  // Checks if the CoordinateSystemContext is complete.
+  //
+  // A complete coordinate system means that the hierarchy eventually traces
+  // back to a parent coordinate system with a `GLOBAL` level. This ensures that
+  // the current context is well-defined and has a global reference.
+  bool IsComplete() const;
+
+  inline bool IsCurrentCartesian() const;
+  inline bool IsCurrentMetric() const;
+
+  bool operator==(const CoordinateSystemContext& other) const;
+  bool operator!=(const CoordinateSystemContext& other) const;
+
+  // Serves as the ultimate reference frame in the hierarchy.
+  std::unique_ptr<FrameNode> current_frame = nullptr;
+
+ private:
+  // A helper function to traverse through the coordinate system hierarchy
+  // (frame chain). This function allows the user to specify a custom operation
+  // to perform on each frame, the traverse stops and returns false when the
+  // operation returns false. Note: ensure the operation to be safe.
+  template <typename Functor>
+  bool Traverse(Functor operation);
+  template <typename Functor>
+  bool Traverse(Functor operation) const;
+};
+
+// Class responsible for converting coordinates between two coordinate system
+// context.
+class CoordinateSystemConverter {
+ public:
+  using FrameNode = CoordinateSystemContext::FrameNode;
+
+  CoordinateSystemConverter(const CoordinateSystemContext& source,
+                            const CoordinateSystemContext& target);
+
+  // Conversion is possible if the source and target coordinate system context
+  // share a common parent frame or are both based on the global frame.
+  inline bool IsConvertable() const;
+
+  // Converts a coordinate from the source coordinate system to the target
+  // coordinate system.
+  Eigen::Vector3d Convert(const Eigen::Vector3d& coordinate) const;
+
+  // Calls `Convert` function.
+  inline Eigen::Vector3d operator()(const Eigen::Vector3d& coordinate) const;
+
+  // Returns a converter reversing the conversion direction: target -> source
+  inline CoordinateSystemConverter Reverse() const;
+
+ private:
+  // Attempts to find the first common parent coordinate system between the
+  // source and target.
+  std::optional<FrameNode> MaybeFirstCommonParent() const;
+
+  const CoordinateSystemContext& source_;
+  const CoordinateSystemContext& target_;
+};
+
+std::ostream& operator<<(std::ostream& stream,
+                         const CoordinateSystemContext& context);
+
+struct PosePrior {
+ public:
+  using CoordinateSystem = CoordinateSystemContext::CoordinateSystem;
   Eigen::Vector3d position =
       Eigen::Vector3d::Constant(std::numeric_limits<double>::quiet_NaN());
   Eigen::Matrix3d position_covariance =
@@ -83,8 +291,89 @@ struct PosePrior {
 
 std::ostream& operator<<(std::ostream& stream, const PosePrior& prior);
 
-bool PosePrior::IsCartesian(CoordinateSystem system) const {
-  return system != CoordinateSystem::WGS84 && system != CoordinateSystem::GRS80;
+bool CoordinateSystemContext::FrameNode::IsCartesian() const {
+  return coordinate_system != CoordinateSystem::WGS84 &&
+         coordinate_system != CoordinateSystem::GRS80 &&
+         coordinate_system != CoordinateSystem::UNDEFINED;
+}
+
+bool CoordinateSystemContext::FrameNode::IsMetric() const {
+  return coordinate_system != CoordinateSystem::MODEL &&
+         coordinate_system != CoordinateSystem::UNDEFINED;
+}
+
+bool CoordinateSystemContext::FrameNode::operator==(
+    const FrameNode& other) const {
+  return coordinate_system == other.coordinate_system &&
+         frame_id == other.frame_id;
+}
+
+bool CoordinateSystemContext::FrameNode::operator!=(
+    const FrameNode& other) const {
+  return !(*this == other);
+}
+
+std::optional<CoordinateSystemContext::CoordinateSystem>
+CoordinateSystemContext::MaybeCurrentCoordinateSystem() const {
+  return current_frame == nullptr
+             ? std::nullopt
+             : std::make_optional(current_frame->coordinate_system);
+}
+
+template <typename Functor>
+bool CoordinateSystemContext::Traverse(Functor operation) {
+  FrameNode* current = current_frame.get();
+
+  while (current != nullptr) {
+    FrameNode* parent = current->parent_frame.get();
+    if (!operation(current)) {
+      return false;
+    }
+    current = parent;
+  }
+  return true;
+}
+
+template <typename Functor>
+bool CoordinateSystemContext::Traverse(Functor operation) const {
+  FrameNode* current = current_frame.get();
+
+  while (current != nullptr) {
+    FrameNode* parent = current->parent_frame.get();
+    if (!operation(current)) {
+      return false;
+    }
+    current = parent;
+  }
+  return true;
+}
+
+std::optional<CoordinateSystemContext::CoordinateSystemLevel>
+CoordinateSystemContext::MaybeCurrentLevel() const {
+  return current_frame == nullptr ? std::nullopt
+                                  : std::make_optional(current_frame->Level());
+}
+
+bool CoordinateSystemContext::IsCurrentCartesian() const {
+  return current_frame->IsCartesian();
+}
+
+bool CoordinateSystemContext::IsCurrentMetric() const {
+  return current_frame->IsMetric();
+}
+
+bool CoordinateSystemConverter::IsConvertable() const {
+  return MaybeFirstCommonParent().has_value() ||
+         (source_.IsComplete() && target_.IsComplete());
+}
+
+Eigen::Vector3d CoordinateSystemConverter::operator()(
+    const Eigen::Vector3d& coordinate) const {
+  return Convert(coordinate);
+}
+
+CoordinateSystemConverter CoordinateSystemConverter::Reverse() const {
+  return CoordinateSystemConverter(target_, source_);
 }
 
 bool PosePrior::operator==(const PosePrior& other) const {
--- src/colmap/geometry/pose_prior_test.cc
+++ src/colmap/geometry/pose_prior_test.cc
@@ -29,10 +29,333 @@
 
 #include "colmap/geometry/pose_prior.h"
 
+#include "colmap/geometry/gps.h"
+#include "colmap/math/math.h"
+#include "colmap/util/eigen_matchers.h"
+
+#include <random>
+
 #include <gtest/gtest.h>
 
 namespace colmap {
 namespace {
+using FramePose = CoordinateSystemContext::FramePose;
+using FrameNode = CoordinateSystemContext::FrameNode;
+using CoordinateSystem = CoordinateSystemContext::CoordinateSystem;
+
+static std::random_device rd;
+static std::mt19937 gen(rd());
+
+int RandomInt(int min = 1, int max = 100) {
+  std::uniform_int_distribution<int> dis(min, max);
+  return dis(gen);
+}
+
+Eigen::Vector3d RandomPosition(double min, double max) {
+  std::uniform_real_distribution<> dis(min, max);
+  return Eigen::Vector3d(dis(gen), dis(gen), dis(gen));
+}
+
+FramePose RandomFramePose() {
+  std::uniform_real_distribution<> pos_dis(-89.0, 89.0);
+  std::uniform_real_distribution<> angle_dis(0.0, 2 * M_PI);
+
+  Eigen::Vector3d random_position(pos_dis(gen), pos_dis(gen), pos_dis(gen));
+  Eigen::Vector3d axis(pos_dis(gen), pos_dis(gen), pos_dis(gen));
+  axis.normalize();
+
+  double angle = angle_dis(gen);
+
+  Eigen::Quaterniond random_orientation(Eigen::AngleAxisd(angle, axis));
+
+  return FramePose{random_orientation, random_position};
+}
+
+TEST(PosePrior, FrameNodeCoordinateConversionSpherical) {
+  // 1. Test for ENU conversion
+  FramePose pose_a_in_b = RandomFramePose();
+
+  CoordinateSystem coordinate_system =
+      RandomInt() % 2 ? CoordinateSystem::WGS84 : CoordinateSystem::GRS80;
+
+  FrameNode frame_b{coordinate_system, FramePose::Identity(), nullptr, 0};
+  FrameNode frame_a{CoordinateSystem::ENU, pose_a_in_b, &frame_b, 0};
+
+  Eigen::Vector3d coordinate_in_a = RandomPosition(-100, 100);
+  Eigen::Vector3d coordinate_in_b_random = RandomPosition(-89, 89);
+
+  // Convert coordinate from frame_a -> frame_b -> frame_a
+  Eigen::Vector3d recovered_in_a =
+      frame_a.ConvertFromParent(frame_a.ConvertToParent(coordinate_in_a));
+  EXPECT_THAT(coordinate_in_a, EigenMatrixNear(recovered_in_a, 1e-8));
+
+  // Convert coordinate from frame_b -> frame_a -> frame_b
+  Eigen::Vector3d recovered_in_b = frame_a.ConvertToParent(
+      frame_a.ConvertFromParent(coordinate_in_b_random));
+  EXPECT_THAT(coordinate_in_b_random, EigenMatrixNear(recovered_in_b, 1e-8));
+
+  // 2. Test for UTM conversion
+  Eigen::Vector3d coordinate_in_c = Eigen::Vector3d(
+      1.91125018424899e5 + 5.0e5, 5.335909515367108e6, 561.1851);
+  Eigen::Vector3d coordinate_in_b_fixed =
+      Eigen::Vector3d(48 + 8.0 / 60 + 51.70361 / 3600,
+                      11 + 34.0 / 60 + 10.51777 / 3600,
+                      561.1851);
+
+  FramePose pose_c_in_b{Eigen::Quaterniond::Identity(), {0, 9, 0}};  // Zone 32
+
+  FrameNode frame_c{CoordinateSystem::UTM, pose_c_in_b, &frame_b, 0};
+
+  // Convert UTM coordinate in frame_c -> frame_b -> frame_c
+  Eigen::Vector3d recovered_in_c =
+      frame_c.ConvertFromParent(frame_c.ConvertToParent(coordinate_in_c));
+  EXPECT_THAT(coordinate_in_c, EigenMatrixNear(recovered_in_c, 1e-8));
+
+  // Convert coordinate from frame_b -> frame_c -> frame_b
+  recovered_in_b =
+      frame_c.ConvertToParent(frame_c.ConvertFromParent(coordinate_in_b_fixed));
+  EXPECT_THAT(coordinate_in_b_fixed, EigenMatrixNear(recovered_in_b, 1e-8));
+}
+
+TEST(PosePrior, FrameNodeCoordinateConversionCartesian) {
+  FramePose pose_a_in_b = RandomFramePose();
+
+  CoordinateSystem model_or_object =
+      RandomInt() % 2 ? CoordinateSystem::MODEL : CoordinateSystem::OBJECT;
+  FrameNode frame_b{model_or_object, FramePose::Identity(), nullptr, 0};
+  FrameNode frame_a{model_or_object, pose_a_in_b, &frame_b, 0};
+
+  Eigen::Vector3d coordinate_in_a = RandomPosition(-100, 100);
+  Eigen::Vector3d coordinate_in_b = RandomPosition(-100, 100);
+
+  Eigen::Vector3d recovered_in_a =
+      frame_a.ConvertFromParent(frame_a.ConvertToParent(coordinate_in_a));
+  EXPECT_THAT(coordinate_in_a, EigenMatrixNear(recovered_in_a, 1e-8));
+
+  Eigen::Vector3d recovered_in_b =
+      frame_a.ConvertFromParent(frame_a.ConvertToParent(coordinate_in_b));
+  EXPECT_THAT(coordinate_in_b, EigenMatrixNear(recovered_in_b, 1e-8));
+}
+
+TEST(PosePrior, CoordinateSystemContextStructure) {
+  CoordinateSystemContext complete_context;
+  CoordinateSystemContext incomplete_context;
+
+  CoordinateSystem wgs84_or_grs80 =
+      RandomInt() % 2 ? CoordinateSystem::WGS84 : CoordinateSystem::GRS80;
+  CoordinateSystem enu_or_utm =
+      RandomInt() % 2 ? CoordinateSystem::ENU : CoordinateSystem::UTM;
+
+  FramePose pose_a_in_b = RandomFramePose();
+  FramePose pose_b_in_c = RandomFramePose();
+  FramePose pose_c_in_d = RandomFramePose();
+
+  FramePose pose_e_in_f = RandomFramePose();
+
+  FrameNode frame_d{wgs84_or_grs80, FramePose::Identity()};
+  FrameNode frame_c{
+      enu_or_utm,
+      pose_c_in_d,
+  };
+  FrameNode frame_b{CoordinateSystem::OBJECT, pose_b_in_c};
+  FrameNode frame_a{CoordinateSystem::OBJECT, pose_a_in_b};
+
+  FrameNode frame_f{CoordinateSystem::MODEL, FramePose::Identity()};
+  FrameNode frame_e{CoordinateSystem::MODEL, pose_e_in_f};
+
+  // WGS84/GRS80 -> UTM/ENU -> OBJECT_1 -> OBJECT_2
+  complete_context.Append(frame_c, FramePose::Identity());
+  complete_context.Append(frame_d, pose_c_in_d);
+  complete_context.Prepend(frame_b, pose_b_in_c);
+  complete_context.Prepend(frame_a, pose_a_in_b);
+
+  // MODEL_1 -> MODEL_2
+  incomplete_context.Append(frame_f, FramePose::Identity());
+  incomplete_context.Prepend(frame_e, pose_e_in_f);
+
+  EXPECT_TRUE(complete_context.IsComplete());
+  EXPECT_FALSE(incomplete_context.IsComplete());
+
+  EXPECT_EQ(complete_context.MaybeBaseFrame().value(), frame_d);
+  EXPECT_EQ(complete_context.MaybePenultimateFrame().value(), frame_c);
+  EXPECT_EQ(incomplete_context.MaybeBaseFrame().value(), frame_f);
+  EXPECT_EQ(incomplete_context.MaybePenultimateFrame().value(), frame_e);
+}
+
+TEST(PosePrior, CoordinateSystemConverterGlobal) {
+  Eigen::Vector3d utm_in_wgs84(0, 9, 0);
+
+  Eigen::Vector3d enu_in_wgs84(48 + 8.0 / 60 + 51.70361 / 3600,
+                               11 + 34.0 / 60 + 10.51777 / 3600,
+                               561.1851);
+
+  // frame_chain_a: object -> utm -> WGS84
+  FramePose object_in_utm = RandomFramePose();
+  std::vector<std::pair<FrameNode, FramePose>> frame_chain_a;
+  frame_chain_a.emplace_back(FrameNode{CoordinateSystem::OBJECT},
+                             FramePose::Identity());
+  frame_chain_a.emplace_back(FrameNode{CoordinateSystem::UTM}, object_in_utm);
+  frame_chain_a.emplace_back(
+      FrameNode{CoordinateSystem::WGS84},
+      FramePose{Eigen::Quaterniond::Identity(), utm_in_wgs84});
+  CoordinateSystemContext context_a(frame_chain_a);
+  context_a.current_frame->frame_id = 0;
+
+  // frame_chain_b: object -> enu -> WGS84
+  FramePose object_in_enu = RandomFramePose();
+  std::vector<std::pair<FrameNode, FramePose>> frame_chain_b;
+  frame_chain_b.emplace_back(FrameNode{CoordinateSystem::OBJECT},
+                             FramePose::Identity());
+  frame_chain_b.emplace_back(FrameNode{CoordinateSystem::ENU}, object_in_enu);
+  frame_chain_b.emplace_back(
+      FrameNode{CoordinateSystem::WGS84},
+      FramePose{Eigen::Quaterniond::Identity(), enu_in_wgs84});
+  CoordinateSystemContext context_b(frame_chain_b);
+  context_b.current_frame->frame_id = 1;
+
+  // We test a same point converted from context a to context b and back.
+  Eigen::Vector3d point_in_wgs84(48 + 8.0 / 60 + 52.40575 / 3600,
+                                 11 + 34.0 / 60 + 11.77179 / 3600,
+                                 561.1509);
+
+  GPSTransform gps_transform(GPSTransform::WGS84);
+  Eigen::Vector3d point_in_enu = gps_transform.EllToENU(
+      point_in_wgs84, enu_in_wgs84[0], enu_in_wgs84[1], enu_in_wgs84[2]);
+
+  // 1.91150201163177e5 + 5.0e5, 5.335932057413140e6, 561.1509
+  Eigen::Vector3d point_in_utm = gps_transform.EllToUTM(point_in_wgs84).first;
+
+  Eigen::Vector3d point_in_a = object_in_utm.ToRigid3d() * point_in_utm;
+  Eigen::Vector3d point_in_b = object_in_enu.ToRigid3d() * point_in_enu;
+
+  CoordinateSystemConverter converter_a_to_b(context_a, context_b);
+
+  Eigen::Vector3d point_in_a_to_b = converter_a_to_b.Convert(point_in_a);
+  EXPECT_THAT(point_in_a_to_b, EigenMatrixNear(point_in_b, 1e-4));  // 0.1mm
+
+  Eigen::Vector3d point_in_b_to_a = converter_a_to_b.Reverse()(point_in_b);
+  EXPECT_THAT(point_in_b_to_a, EigenMatrixNear(point_in_a, 1e-4));
+}
+
+TEST(PosePrior, CoordinateSystemConverterCommonParent) {
+  Eigen::Vector3d utm_in_wgs84(0, 9, 0);
+
+  Eigen::Vector3d enu_in_wgs84(48 + 8.0 / 60 + 51.70361 / 3600,
+                               11 + 34.0 / 60 + 10.51777 / 3600,
+                               561.1851);
+
+  // We test a same point converted from context a to context b and back.
+  Eigen::Vector3d point_in_wgs84(48 + 8.0 / 60 + 52.40575 / 3600,
+                                 11 + 34.0 / 60 + 11.77179 / 3600,
+                                 561.1509);
+
+  GPSTransform gps_transform(GPSTransform::WGS84);
+  Eigen::Vector3d point_in_enu = gps_transform.EllToENU(
+      point_in_wgs84, enu_in_wgs84[0], enu_in_wgs84[1], enu_in_wgs84[2]);
+
+  // 1.91150201163177e5 + 5.0e5, 5.335932057413140e6, 561.1509
+  Eigen::Vector3d point_in_utm = gps_transform.EllToUTM(point_in_wgs84).first;
+
+  FramePose object_0_in_object_utm = RandomFramePose();
+  FramePose object_0_in_object_enu =
+      FramePose::Identity();  // Set object0 same as the enu
+  FramePose object_1_in_object_2 = RandomFramePose();
+  FramePose object_2_in_object_0 = RandomFramePose();
+  FramePose object_3_in_object_4 = RandomFramePose();
+  FramePose object_4_in_object_0 = RandomFramePose();
+
+  // frame_chain_a: object_1 <- object_2 <- object_0 <- enu
+  std::vector<std::pair<FrameNode, FramePose>> frame_chain_a;
+  frame_chain_a.emplace_back(FrameNode(CoordinateSystem::OBJECT, 1),
+                             FramePose::Identity());
+  frame_chain_a.emplace_back(FrameNode(CoordinateSystem::OBJECT, 2),
+                             object_1_in_object_2);
+  frame_chain_a.emplace_back(FrameNode(CoordinateSystem::OBJECT, 0),
+                             object_2_in_object_0);
+  frame_chain_a.emplace_back(FrameNode{CoordinateSystem::ENU},
+                             object_0_in_object_enu);
+  CoordinateSystemContext context_a(frame_chain_a);
+
+  // frame_chain_b: object_3 <- object_4 <- object_0 <- utm
+  std::vector<std::pair<FrameNode, FramePose>> frame_chain_b;
+  frame_chain_b.emplace_back(FrameNode(CoordinateSystem::OBJECT, 3),
+                             FramePose::Identity());
+  frame_chain_b.emplace_back(FrameNode(CoordinateSystem::OBJECT, 4),
+                             object_3_in_object_4);
+  frame_chain_b.emplace_back(FrameNode(CoordinateSystem::OBJECT, 0),
+                             object_4_in_object_0);
+  frame_chain_b.emplace_back(FrameNode{CoordinateSystem::UTM},
+                             object_0_in_object_utm);
+  CoordinateSystemContext context_b(frame_chain_b);
+
+  CoordinateSystemConverter converter_a_to_b(context_a, context_b);
+
+  Eigen::Vector3d point_in_object_0 =
+      object_0_in_object_enu.ToRigid3d() * point_in_enu;
+  Eigen::Vector3d point_in_object_1 = object_1_in_object_2.ToRigid3d() *
+                                      object_2_in_object_0.ToRigid3d() *
+                                      point_in_object_0;
+  Eigen::Vector3d point_in_object_3 = object_3_in_object_4.ToRigid3d() *
+                                      object_4_in_object_0.ToRigid3d() *
+                                      point_in_object_0;
+
+  Eigen::Vector3d point_in_a_to_b = converter_a_to_b.Convert(point_in_object_1);
+  EXPECT_THAT(point_in_a_to_b, EigenMatrixNear(point_in_object_3, 1e-8));
+
+  Eigen::Vector3d point_in_b_to_a =
+      converter_a_to_b.Reverse()(point_in_object_3);
+  EXPECT_THAT(point_in_b_to_a, EigenMatrixNear(point_in_object_1, 1e-8));
+}
+
+TEST(PosePrior, CoordinateSystemConverterCommonParentVirtual) {
+  FramePose model_4_in_model_3 = RandomFramePose();
+  FramePose model_3_in_model_2 = RandomFramePose();
+  FramePose model_2_in_model_0 = RandomFramePose();
+  FramePose model_6_in_model_5 = RandomFramePose();
+  FramePose model_5_in_model_2 = RandomFramePose();
+  FramePose model_2_in_model_1 = RandomFramePose();
+
+  // frame chain a: model_0 -> model_2 -> model_3 -> model_4
+  std::vector<std::pair<FrameNode, FramePose>> frame_chain_a;
+  frame_chain_a.emplace_back(FrameNode(CoordinateSystem::MODEL, 4),
+                             FramePose::Identity());
+  frame_chain_a.emplace_back(FrameNode(CoordinateSystem::MODEL, 3),
+                             model_4_in_model_3);
+  frame_chain_a.emplace_back(FrameNode(CoordinateSystem::MODEL, 2),
+                             model_3_in_model_2);
+  frame_chain_a.emplace_back(FrameNode(CoordinateSystem::MODEL, 0),
+                             model_2_in_model_0);
+  CoordinateSystemContext context_a(frame_chain_a);
+
+  // frame chain b: model_1 -> model_2 -> model_5 -> model_6
+  std::vector<std::pair<FrameNode, FramePose>> frame_chain_b;
+  frame_chain_b.emplace_back(FrameNode(CoordinateSystem::MODEL, 6),
+                             FramePose::Identity());
+  frame_chain_b.emplace_back(FrameNode(CoordinateSystem::MODEL, 5),
+                             model_6_in_model_5);
+  frame_chain_b.emplace_back(FrameNode(CoordinateSystem::MODEL, 2),
+                             model_5_in_model_2);
+  frame_chain_b.emplace_back(FrameNode(CoordinateSystem::MODEL, 1),
+                             model_2_in_model_1);
+  CoordinateSystemContext context_b(frame_chain_b);
+
+  CoordinateSystemConverter converter_a_to_b(context_a, context_b);
+
+  Eigen::Vector3d point_in_model_2 = RandomPosition(-100, 100);
+  Eigen::Vector3d point_in_model_4 = model_4_in_model_3.ToRigid3d() *
+                                     model_3_in_model_2.ToRigid3d() *
+                                     point_in_model_2;
+  Eigen::Vector3d point_in_model_6 = model_6_in_model_5.ToRigid3d() *
+                                     model_5_in_model_2.ToRigid3d() *
+                                     point_in_model_2;
+
+  Eigen::Vector3d point_in_a_to_b = converter_a_to_b.Convert(point_in_model_4);
+  EXPECT_THAT(point_in_a_to_b, EigenMatrixNear(point_in_model_6, 1e-8));
+
+  Eigen::Vector3d point_in_b_to_a =
+      converter_a_to_b.Reverse()(point_in_model_6);
+  EXPECT_THAT(point_in_b_to_a, EigenMatrixNear(point_in_model_4, 1e-8));
+}
 
 TEST(PosePrior, Equals) {
   PosePrior prior;
@@ -56,7 +379,7 @@ TEST(PosePrior, Print) {
   stream << prior;
   EXPECT_EQ(stream.str(),
             "PosePrior(position=[0, 0, 0], position_covariance=[1, 0, 0, 0, 1, "
-            "0, 0, 0, 1], coordinate_system=CARTESIAN)");
+            "0, 0, 0, 1], coordinate_system=ECEF)");
 }
 
 }  // namespace
