b1f719cad52f43ba56879d78e3286234a30c4adc
Johannes Schönberger <jsch@meta.com>
2025-02-26 17:04:58 +0100

    Python bindings for visual index

--- src/pycolmap/main.cc
+++ src/pycolmap/main.cc
@@ -21,6 +21,7 @@ void BindGeometry(py::module& m);
 void BindImage(py::module& m);
 void BindOptim(py::module& m);
 void BindPipeline(py::module& m);
+void BindRetrieval(py::module& m);
 void BindScene(py::module& m);
 void BindSensor(py::module& m);
 void BindSfm(py::module& m);
@@ -52,6 +53,7 @@ PYBIND11_MODULE(_core, m) {
   BindSensor(m);
   BindImage(m);
   BindEstimators(m);
+  BindRetrieval(m);
   BindSfm(m);
   BindSift(m);
   BindPipeline(m);
