0287d8d11621fbc84ce6f32e285dcdd186652b71
Johannes Schönberger <jsch@meta.com>
2025-05-26 20:55:09 +0200

    d

--- src/colmap/retrieval/visual_index.h
+++ src/colmap/retrieval/visual_index.h
@@ -138,7 +138,7 @@ class VisualIndex {
   // Read and write the visual index. This can be done for an index with and
   // without indexed images.
   static std::unique_ptr<VisualIndex> Read(const std::string& vocab_tree_path,
-                                           bool legacy_flann = false);
+                                           bool legacy_flann = true);
   virtual void Write(const std::string& path) const = 0;
 
  protected:
