cc81c3f6a2625de1319105b61369a201df912f9f
Johannes Schönberger <jsch@meta.com>
2025-07-24 19:15:47 +0200

    d

--- src/pycolmap/feature/types.cc
+++ src/pycolmap/feature/types.cc
@@ -40,7 +40,7 @@ void BindFeatureTypes(py::module& m) {
             return ss.str();
           });
   MakeDataclass(PyFeatureKeypoint);
-  py::bind_vector<FeatureKeypoints>(m, "FeatureKeypoints");
+  py::bind_vector<FeatureKeypoints>(m, "FeatureKeypointList");
 
   auto PyFeatureMatch =
       py::class_<FeatureMatch>(m, "FeatureMatch")
@@ -55,5 +55,5 @@ void BindFeatureTypes(py::module& m) {
             return ss.str();
           });
   MakeDataclass(PyFeatureMatch);
-  py::bind_vector<FeatureMatches>(m, "FeatureMatches");
+  py::bind_vector<FeatureMatches>(m, "FeatureMatchList");
 }
