396af6a16c363298153c9a593e1268569e3986df
StonerLing <sisyphus.stoner@outlook.com>
2025-07-29 15:06:03 +0800

    Update BindPosePrior

--- src/pycolmap/geometry/pose_prior.cc
+++ src/pycolmap/geometry/pose_prior.cc
@@ -9,12 +9,13 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-using namespace colmap;
-using namespace pybind11::literals;
+using namespace colmap;              // NOLINT
+using namespace pybind11::literals;  // NOLINT
 namespace py = pybind11;
 
 void BindPosePrior(py::module& m) {
   using PosePriorCoordinateSystem = PosePrior::CoordinateSystem;
+
   py::enum_<PosePriorCoordinateSystem> PyCoordinateSystem(
       m, "PosePriorCoordinateSystem");
   PyCoordinateSystem.value("UNDEFINED", PosePriorCoordinateSystem::UNDEFINED)
@@ -37,10 +38,60 @@ void BindPosePrior(py::module& m) {
            "position"_a,
            "position_covariance"_a,
            "coordinate_system"_a)
-      .def_readwrite("position", &PosePrior::position)
-      .def_readwrite("position_covariance", &PosePrior::position_covariance)
+
+      .def(py::init<const Eigen::Vector3d&, const Eigen::Quaterniond&>(),
+           "position"_a,
+           "rotation"_a)
+      .def(py::init<const Eigen::Vector3d&,
+                    const Eigen::Quaterniond&,
+                    const Eigen::Matrix3d&,
+                    const Eigen::Matrix3d&>(),
+           "position"_a,
+           "rotation"_a,
+           "position_covariance"_a,
+           "rotation_covariance"_a)
+      .def(py::init<const Rigid3d&>(), "cam_from_world"_a)
+      .def(py::init<const Rigid3d&,
+                    const Eigen::Matrix3d&,
+                    const Eigen::Matrix3d&>(),
+           "cam_from_world"_a,
+           "position_covariance"_a,
+           "rotation_covariance"_a)
+
       .def_readwrite("coordinate_system", &PosePrior::coordinate_system)
-      .def("is_valid", &PosePrior::IsValid)
-      .def("is_covariance_valid", &PosePrior::IsCovarianceValid);
+      .def_readwrite("cam_from_world", &PosePrior::cam_from_world)
+
+      .def("position", &PosePrior::Position)
+      .def("set_position", &PosePrior::SetPosition)
+      .def("rotation", &PosePrior::Rotation)
+      .def("set_rotation", &PosePrior::SetRotation)
+      .def("set_rotation_from_coeffs", &PosePrior::SetRotationFromCoeffs)
+      .def("translation", &PosePrior::Translation)
+      .def("set_translation", &PosePrior::SetTranslation)
+
+      .def("translation_covariance", &PosePrior::TranslationCovariance)
+      .def("set_translation_covariance", &PosePrior::SetTranslationCovariance)
+      .def("rotation_covariance", &PosePrior::RotationCovariance)
+      .def("set_rotation_covariance", &PosePrior::SetRotationCovariance)
+      .def("position_covariance", &PosePrior::PositionCovariance)
+      .def("set_position_covariance", &PosePrior::SetPositionCovariance)
+      .def("pose_covariance", &PosePrior::PoseCovariance)
+      .def("set_pose_covariance", &PosePrior::SetPoseCovariance)
+
+      .def("has_valid_translation", &PosePrior::HasValidTranslation)
+      .def("has_valid_translation_covariance",
+           &PosePrior::HasValidTranslationCovariance)
+      .def("has_valid_rotation", &PosePrior::HasValidRotation)
+      .def("has_valid_rotation_covariance",
+           &PosePrior::HasValidRotationCovariance)
+      .def("has_valid_position", &PosePrior::HasValidPosition)
+      .def("has_valid_position_covariance",
+           &PosePrior::HasValidPositionCovariance)
+      .def("has_valid_pose", &PosePrior::HasValidPose)
+      .def("has_valid_pose_covariance", &PosePrior::HasValidPoseCovariance)
+
+      .def("__eq__", &PosePrior::operator==)
+      .def("__ne__", &PosePrior::operator!=);
+
   MakeDataclass(PyPosePrior);
 }
