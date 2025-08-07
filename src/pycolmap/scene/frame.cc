dcf83fbd1c09bae11d1e0d5f9a628666a0244374
Shaohui Liu <b1ueber2y@gmail.com>
2025-08-07 10:22:11 +0200

    format

--- src/pycolmap/scene/frame.cc
+++ src/pycolmap/scene/frame.cc
@@ -29,7 +29,9 @@ void BindFrame(py::module& m) {
                     &Frame::SetRigId,
                     "Unique identifier of the rig.")
       .def("add_data_id", &Frame::AddDataId, "Associate data with frame.")
-      .def("num_data_ids", &Frame::NumData, "Number of associated data items in frame.")
+      .def("num_data_ids",
+           &Frame::NumDataIds,
+           "Number of associated data items in frame.")
       .def("has_data",
            &Frame::HasDataId,
            "Check whether frame has associated data.")
