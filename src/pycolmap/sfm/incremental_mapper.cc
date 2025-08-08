cd3bccd9017c254acda05a6f4836a7d055547f62
Stoner Ling <128291220+StonerLing@users.noreply.github.com>
2025-08-08 19:51:44 +0800

    Add random_seed to IncrementalMapper to control all RANSAC seeds for improved SfM reproducibility (#3544)
    This PR introduces a `random_seed` option in IncrementalMapper that
    controls the random seeds used in all RANSAC processes throughout the
    reconstruction pipeline. This enhancement improves SfM reproducibility,
    facilitating debugging and consistent testing.
    
    Floating-point variations from Ceres optimization in multithreaded
    environments remain unavoidable.
    
    ---------
    
    Co-authored-by: StonerLing <sisyphus.stoner@outlook.com>
    Co-authored-by: Johannes Schönberger <jsch@demuc.de>

--- src/pycolmap/sfm/incremental_mapper.cc
+++ src/pycolmap/sfm/incremental_mapper.cc
@@ -62,6 +62,10 @@ void BindIncrementalPipeline(py::module& m) {
       .def_readwrite("num_threads",
                      &Opts::num_threads,
                      "The number of threads to use during reconstruction.")
+      .def_readwrite(
+          "random_seed",
+          &Opts::random_seed,
+          "PRNG seed for all stochastic methods during reconstruction.")
       .def_readwrite("min_focal_length_ratio",
                      &Opts::min_focal_length_ratio,
                      "The threshold used to filter and ignore images with "
@@ -339,6 +343,10 @@ void BindIncrementalMapperOptions(py::module& m) {
                      "If reconstruction is provided as input, fix the existing "
                      "frame poses.")
       .def_readwrite("num_threads", &Opts::num_threads, "Number of threads.")
+      .def_readwrite(
+          "random_seed",
+          &Opts::random_seed,
+          "PRNG seed for all stochastic methods during reconstruction.")
       .def_readwrite("image_selection_method",
                      &Opts::image_selection_method,
                      "Method to find and select next best image to register.")
