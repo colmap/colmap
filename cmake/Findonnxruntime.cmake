2c27d148c5d1f181639b5353e02f999ba642429a
Johannes Schönberger <jsch@meta.com>
2025-09-03 18:55:08 +0200

    d

--- cmake/Findonnxruntime.cmake
+++ cmake/Findonnxruntime.cmake
@@ -73,7 +73,6 @@ else()
         /sw/lib
         /opt/lib
         /opt/local/lib)
-    message(STATUS ${onnxruntime_LIBRARY_DIR_HINTS})
     find_library(onnxruntime_LIBRARIES
         NAMES onnxruntime libonnxruntime
         PATHS ${onnxruntime_LIBRARY_DIR_HINTS})
