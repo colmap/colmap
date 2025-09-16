65afdf255f113ac126cb13a9a8fe8ba687576e5a
Shaohui Liu <b1ueber2y@gmail.com>
2025-09-16 13:59:24 +0200

    update pybind.

--- src/pycolmap/sensor/rig.cc
+++ src/pycolmap/sensor/rig.cc
@@ -39,7 +39,12 @@ void BindSensorRig(py::module& m) {
       .def_property_readonly("ref_sensor_id",
                              &Rig::RefSensorId,
                              "The reference sensor's identifier.")
-      .def("sensor_ids", &Rig::SensorIds, "Get all sensor ids in the rig.")
+      .def("sensor_ids",
+           &Rig::SensorIds,
+           "Get all sensor ids except for the reference sensor in the rig.")
+      .def("all_sensor_ids",
+           &Rig::AllSensorIds,
+           "Get all sensor ids (including the reference sensor) in the rig.")
       .def(
           "sensor_from_rig",
           [](Rig& self, sensor_t sensor_id) -> py::typing::Optional<Rigid3d> {
