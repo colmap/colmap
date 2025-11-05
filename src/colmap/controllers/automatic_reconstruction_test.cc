63ff84d9c9d2477fd96fc6cd7302c92c9fd57977
Johannes Schönberger <jsch@meta.com>
2025-11-05 17:05:37 +0100

    Create unit test for automatic reconstruction controller

---
+++ src/colmap/controllers/automatic_reconstruction_test.cc
@@ -0,0 +1,85 @@
+// Copyright (c), ETH Zurich and UNC Chapel Hill.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of ETH Zurich and UNC Chapel Hill nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "colmap/controllers/automatic_reconstruction.h"
+
+#include "colmap/scene/reconstruction_manager.h"
+#include "colmap/scene/reconstruction_matchers.h"
+#include "colmap/scene/synthetic.h"
+#include "colmap/util/file.h"
+#include "colmap/util/testing.h"
+
+#include <gtest/gtest.h>
+
+namespace colmap {
+namespace {
+
+TEST(AutomaticReconstructionController, Nominal) {
+  const std::string test_dir = CreateTestDir();
+  const std::string workspace_path = test_dir + "/workspace";
+  const std::string image_path = test_dir + "/images";
+  CreateDirIfNotExists(workspace_path);
+  CreateDirIfNotExists(image_path);
+
+  Reconstruction gt_reconstruction;
+  SyntheticDatasetOptions synthetic_dataset_options;
+  synthetic_dataset_options.num_rigs = 5;
+  synthetic_dataset_options.num_cameras_per_rig = 1;
+  synthetic_dataset_options.num_frames_per_rig = 1;
+  synthetic_dataset_options.num_points3D = 100;
+  synthetic_dataset_options.num_points2D_without_point3D = 10;
+  SynthesizeDataset(synthetic_dataset_options, &gt_reconstruction);
+  SynthesizeImages(SyntheticImageOptions(), gt_reconstruction, image_path);
+
+  AutomaticReconstructionController::Options options;
+  options.workspace_path = workspace_path;
+  options.image_path = image_path;
+  options.data_type = AutomaticReconstructionController::DataType::INDIVIDUAL;
+  options.quality = AutomaticReconstructionController::Quality::LOW;
+  options.single_camera = false;
+  options.dense = false;  // Disable dense reconstruction to avoid GPU
+  options.use_gpu = false;
+
+  auto reconstruction_manager = std::make_shared<ReconstructionManager>();
+  AutomaticReconstructionController controller(options, reconstruction_manager);
+  controller.Start();
+  controller.Wait();
+
+  EXPECT_EQ(reconstruction_manager->Size(), 1);
+  EXPECT_THAT(*reconstruction_manager->Get(0),
+              ReconstructionNear(gt_reconstruction,
+                                 /*max_rotation_error_deg=*/0.3,
+                                 /*max_proj_center_error=*/0.1,
+                                 /*max_scale_error=*/std::nullopt,
+                                 /*num_obs_tolerance=*/0.9,
+                                 /*align=*/true));
+}
+
+}  // namespace
+}  // namespace colmap
