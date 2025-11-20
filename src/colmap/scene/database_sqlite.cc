795abab486f5f6433c6cfb8fd24cf568ef3eb001
Johannes Schönberger <jsch@meta.com>
2025-11-20 15:49:56 +0100

    Inline SQLite3 utils in database implementation

--- src/colmap/scene/database_sqlite.cc
+++ src/colmap/scene/database_sqlite.cc
@@ -29,11 +29,41 @@
 
 #include "colmap/scene/database.h"
 #include "colmap/util/endian.h"
-#include "colmap/util/sqlite3_utils.h"
 #include "colmap/util/string.h"
+
+#include <sqlite3.h>
 
 namespace colmap {
 namespace {
+
+inline int SQLite3CallHelper(int result_code,
+                             const std::string& filename,
+                             int line) {
+  switch (result_code) {
+    case SQLITE_OK:
+    case SQLITE_ROW:
+    case SQLITE_DONE:
+      return result_code;
+    default:
+      LogMessageFatalThrow<std::runtime_error>(filename.c_str(), line).stream()
+          << "SQLite error: " << sqlite3_errstr(result_code);
+      return result_code;
+  }
+}
+
+#define SQLITE3_CALL(func) colmap::SQLite3CallHelper(func, __FILE__, __LINE__)
+
+#define SQLITE3_EXEC(database, sql, callback)                             \
+  {                                                                       \
+    char* err_msg = nullptr;                                              \
+    const int result_code = sqlite3_exec(                                 \
+        THROW_CHECK_NOTNULL(database), sql, callback, nullptr, &err_msg); \
+    if (result_code != SQLITE_OK) {                                       \
+      LOG(ERROR) << "SQLite error [" << __FILE__ << ", line " << __LINE__ \
+                 << "]: " << err_msg;                                     \
+      sqlite3_free(err_msg);                                              \
+    }                                                                     \
+  }
 
 struct Sqlite3StmtContext {
   explicit Sqlite3StmtContext(sqlite3_stmt* sql_stmt)
