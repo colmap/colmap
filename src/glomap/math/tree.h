404bbe1353e60768a469e1d8acde643f9773649a
Johannes Schönberger <jsch@meta.com>
2025-11-24 11:00:21 +0100

    Misc code quality improvements for max spanning tree

--- src/glomap/math/tree.h
+++ src/glomap/math/tree.h
@@ -1,16 +1,11 @@
+#pragma once
 
-#include <unordered_set>
-
-#include <glomap/scene/types_sfm.h>
+#include "glomap/scene/image.h"
+#include "glomap/scene/view_graph.h"
 
 namespace glomap {
-enum WeightType { INLIER_NUM, INLIER_RATIO };
 
-// Return the number of nodes in the tree
-int BFS(const std::vector<std::vector<int>>& graph,
-        int root,
-        std::vector<int>& parents,
-        std::vector<std::pair<int, int>> banned_edges = {});
+enum class WeightType { INLIER_NUM, INLIER_RATIO };
 
 image_t MaximumSpanningTree(const ViewGraph& view_graph,
                             const std::unordered_map<image_t, Image>& images,
