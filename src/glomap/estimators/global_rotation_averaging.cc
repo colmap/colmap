c6ac8ba39e2528a454f64bbc960b3bf667336b72
Johannes Schönberger <jsch@demuc.de>
2025-11-24 11:44:50 +0100

    Misc code quality improvements for max spanning tree (#3755)

--- src/glomap/estimators/global_rotation_averaging.cc
+++ src/glomap/estimators/global_rotation_averaging.cc
@@ -92,7 +92,7 @@ void RotationEstimator::InitializeFromMaximumSpanningTree(
   // Here, we assume that largest connected component is already retrieved, so
   // we do not need to do that again compute maximum spanning tree.
   std::unordered_map<image_t, image_t> parents;
-  image_t root = MaximumSpanningTree(view_graph, images, parents, INLIER_NUM);
+  const image_t root = MaximumSpanningTree(view_graph, images, parents, WeightType::INLIER_NUM);
 
   // Iterate through the tree to initialize the rotation
   // Establish child info
