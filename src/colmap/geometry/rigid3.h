4811b34c953110761f407a1681274a4654357ac5
Johannes Schönberger <jsch@meta.com>
2025-11-28 15:10:51 +0100

    d

--- src/colmap/geometry/rigid3.h
+++ src/colmap/geometry/rigid3.h
@@ -98,6 +98,7 @@ inline Rigid3d Inverse(const Rigid3d& b_from_a) {
   return a_from_b;
 }
 
+// Return the origin position of the target frame in the source frame.
 inline Eigen::Vector3d TgtOriginInSrc(const Rigid3d& b_from_a) {
   return b_from_a.rotation.inverse() * -b_from_a.translation;
 }
