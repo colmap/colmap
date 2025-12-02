dd4c3481868540bcfedec2547b7c7cb1aaf32eea
Johannes Schönberger <jsch@demuc.de>
2025-12-02 13:55:41 +0100

    Enable compiler warnings as errors in GCC/CLang CI builds (#3788)
    Not enabling for MSVC, because it triggers too many warnings right now.

--- src/colmap/exe/sfm.cc
+++ src/colmap/exe/sfm.cc
@@ -334,7 +334,7 @@ int RunMapper(int argc, char** argv) {
   }
 
   if (input_path.empty()) {
-    for (size_t i = 0; i < reconstruction_manager->Size(); i++) {
+    for (size_t i = 0; i < reconstruction_manager->Size(); ++i) {
       const std::string reconstruction_path =
           JoinPaths(output_path, std::to_string(i));
       options.Write(JoinPaths(reconstruction_path, "project.ini"));
@@ -452,7 +452,7 @@ int RunPosePriorMapper(int argc, char** argv) {
   }
 
   if (input_path.empty()) {
-    for (size_t i = 0; i < reconstruction_manager->Size(); i++) {
+    for (size_t i = 0; i < reconstruction_manager->Size(); ++i) {
       const std::string reconstruction_path =
           JoinPaths(output_path, std::to_string(i));
       options.Write(JoinPaths(reconstruction_path, "project.ini"));
