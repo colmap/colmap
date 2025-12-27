749c850170ce6a55ddab9da7ae4e2dfc6946ba2d
Shaohui Liu <b1ueber2y@gmail.com>
2025-12-27 20:07:28 +0100

    update pybind comment to be consistent with c++.

--- src/pycolmap/scene/reconstruction.cc
+++ src/pycolmap/scene/reconstruction.cc
@@ -105,8 +105,7 @@ void BindReconstruction(py::module& m) {
       .def("exists_point3D", &Reconstruction::ExistsPoint3D, "point3D_id"_a)
       .def("check_is_valid",
            &Reconstruction::CheckIsValid,
-           "Check whether the reconstruction is internally consistent. "
-           "Validates pointers, registered frames, and 2D-3D associations.")
+           "Check whether the reconstruction object is internally consistent.")
       .def("load", &Reconstruction::Load, "database_cache"_a)
       .def("tear_down", &Reconstruction::TearDown)
       .def("add_rig", &Reconstruction::AddRig, "rig"_a, "Add new rig.")
