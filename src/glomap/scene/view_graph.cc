41b8479c6d1fb70fe3755549f3913e3f25d68606
Shaohui Liu <b1ueber2y@gmail.com>
2025-12-27 05:29:45 +0100

    Move relative pose filtering utilities into ViewGraph. (#3864)

--- src/glomap/scene/view_graph.cc
+++ src/glomap/scene/view_graph.cc
@@ -157,4 +157,67 @@ ViewGraph::CreateFrameAdjacencyList(
   return adjacency_list;
 }
 
+void ViewGraph::FilterByRelativeRotation(
+    const colmap::Reconstruction& reconstruction, double max_angle_deg) {
+  const double max_angle_rad = colmap::DegToRad(max_angle_deg);
+  int num_invalid = 0;
+  for (auto& [pair_id, image_pair] : image_pairs) {
+    if (!image_pair.is_valid) continue;
+
+    const Image& image1 = reconstruction.Image(image_pair.image_id1);
+    const Image& image2 = reconstruction.Image(image_pair.image_id2);
+
+    if (!image1.HasPose() || !image2.HasPose()) {
+      continue;
+    }
+
+    const Eigen::Quaterniond cam2_from_cam1 =
+        image2.CamFromWorld().rotation *
+        image1.CamFromWorld().rotation.inverse();
+    if (cam2_from_cam1.angularDistance(image_pair.cam2_from_cam1.rotation) >
+        max_angle_rad) {
+      image_pair.is_valid = false;
+      num_invalid++;
+    }
+  }
+
+  LOG(INFO) << "Marked " << num_invalid
+            << " image pairs as invalid with relative rotation error > "
+            << max_angle_deg << " degrees";
+}
+
+void ViewGraph::FilterByNumInliers(int min_num_inliers) {
+  int num_invalid = 0;
+  for (auto& [pair_id, image_pair] : image_pairs) {
+    if (!image_pair.is_valid) continue;
+
+    if (image_pair.inliers.size() < min_num_inliers) {
+      image_pair.is_valid = false;
+      num_invalid++;
+    }
+  }
+
+  LOG(INFO) << "Marked " << num_invalid
+            << " image pairs as invalid with inlier count < "
+            << min_num_inliers;
+}
+
+void ViewGraph::FilterByInlierRatio(double min_inlier_ratio) {
+  int num_invalid = 0;
+  for (auto& [pair_id, image_pair] : image_pairs) {
+    if (!image_pair.is_valid) continue;
+
+    const double inlier_ratio = image_pair.inliers.size() /
+                                static_cast<double>(image_pair.matches.rows());
+    if (inlier_ratio < min_inlier_ratio) {
+      image_pair.is_valid = false;
+      num_invalid++;
+    }
+  }
+
+  LOG(INFO) << "Marked " << num_invalid
+            << " image pairs as invalid with inlier ratio < "
+            << min_inlier_ratio;
+}
+
 }  // namespace glomap
