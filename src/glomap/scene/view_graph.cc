colmap/colmap

commit 374128bd6300a29b7fff6a384c6a94eb1c758eff

Towards removing custom glomap image class (#3805)
First step in removing the custom class. Next steps will then remove the
features/features_undist fields as well as refactor the current "misuse"
of "IsRegistered/HasPose" for flagging images in the largest connected
component.

src/glomap/scene/view_graph.cc
@@ -55,69 +55,70 @@ std::vector<std::unordered_set<frame_t>> FindConnectedComponents(
 
 int ViewGraph::KeepLargestConnectedComponents(
     std::unordered_map<frame_t, Frame>& frames,
-    std::unordered_map<image_t, Image>& images) {
+    const std::unordered_map<image_t, Image>& images) {
   const std::vector<std::unordered_set<frame_t>> connected_components =
       FindConnectedComponents(CreateFrameAdjacencyList(images));
 
-  int max_idx = -1;
-  int max_img = 0;
-  for (int comp = 0; comp < connected_components.size(); comp++) {
-    if (connected_components[comp].size() > max_img) {
-      max_img = connected_components[comp].size();
-      max_idx = comp;
+  int max_comp = -1;
+  int max_num_reg_images = 0;
+  for (int comp = 0; comp < connected_components.size(); ++comp) {
+    if (connected_components[comp].size() > max_num_reg_images) {
+      int num_reg_images = 0;
+      for (auto& [image_id, image] : images) {
+        if (image.HasPose()) {
+          ++num_reg_images;
+        }
+      }
+      if (num_reg_images > max_num_reg_images)
+        max_num_reg_images = connected_components[comp].size();
+      max_comp = comp;
     }
   }
 
-  if (max_img == 0) return 0;
+  if (max_comp == -1) {
+    return 0;
+  }
 
   const std::unordered_set<frame_t>& largest_component =
-      connected_components[max_idx];
-
-  // Set all frames to not registered
+      connected_components[max_comp];
   for (auto& [frame_id, frame] : frames) {
-    frame.is_registered = false;
+    if (largest_component.count(frame_id) == 0) {
+      frame.ResetPose();
+    }
   }
-  // Set the frames in the largest component to registered
-  for (auto frame_id : largest_component) {
-    frames[frame_id].is_registered = true;
-  }
-  // set all pairs not in the largest component to invalid
+
   for (auto& [pair_id, image_pair] : image_pairs) {
-    if (!images[image_pair.image_id1].IsRegistered() ||
-        !images[image_pair.image_id2].IsRegistered()) {
+    if (!images.at(image_pair.image_id1).HasPose() ||
+        !images.at(image_pair.image_id2).HasPose()) {
       image_pair.is_valid = false;
     }
   }
 
-  max_img = 0;
-  for (auto& [image_id, image] : images) {
-    if (image.IsRegistered()) max_img++;
-  }
-  return max_img;
+  return max_num_reg_images;
 }
 
 int ViewGraph::MarkConnectedComponents(
     std::unordered_map<frame_t, Frame>& frames,
-    std::unordered_map<image_t, Image>& images,
-    int min_num_img) {
+    const std::unordered_map<image_t, Image>& images,
+    int min_num_images) {
   const std::vector<std::unordered_set<frame_t>> connected_components =
       FindConnectedComponents(CreateFrameAdjacencyList(images));
   const int num_comp = connected_components.size();
 
-  std::vector<std::pair<int, int>> cluster_num_img(num_comp);
+  std::vector<std::pair<int, int>> comp_num_images(num_comp);
   for (int comp = 0; comp < num_comp; comp++) {
-    cluster_num_img[comp] =
+    comp_num_images[comp] =
         std::make_pair(connected_components[comp].size(), comp);
   }
-  std::sort(cluster_num_img.begin(), cluster_num_img.end(), std::greater<>());
+  std::sort(comp_num_images.begin(), comp_num_images.end(), std::greater<>());
 
   // Set the cluster number of every frame to be -1
   for (auto& [frame_id, frame] : frames) frame.cluster_id = -1;
 
   int comp = 0;
   for (; comp < num_comp; comp++) {
-    if (cluster_num_img[comp].first < min_num_img) break;
-    for (auto frame_id : connected_components[cluster_num_img[comp].second]) {
+    if (comp_num_images[comp].first < min_num_images) break;
+    for (auto frame_id : connected_components[comp_num_images[comp].second]) {
       frames[frame_id].cluster_id = comp;
     }
   }
@@ -143,8 +144,8 @@ ViewGraph::CreateFrameAdjacencyList(
   std::unordered_map<frame_t, std::unordered_set<frame_t>> adjacency_list;
   for (const auto& [_, image_pair] : image_pairs) {
     if (image_pair.is_valid) {
-      const frame_t frame_id1 = images.at(image_pair.image_id1).frame_id;
-      const frame_t frame_id2 = images.at(image_pair.image_id2).frame_id;
+      const frame_t frame_id1 = images.at(image_pair.image_id1).FrameId();
+      const frame_t frame_id2 = images.at(image_pair.image_id2).FrameId();
       adjacency_list[frame_id1].insert(frame_id2);
       adjacency_list[frame_id2].insert(frame_id1);
     }

src/glomap/sfm/track_retriangulation.cc
@@ -50,11 +50,10 @@ bool RetriangulateTracks(const TriangulatorOptions& options,
   // Check whether the image is in the database cache. If not, set the image
   // as not registered to avoid memory error.
   std::vector<image_t> image_ids_notconnected;
-  for (auto& image : images) {
-    if (!database_cache->ExistsImage(image.first) &&
-        image.second.IsRegistered()) {
-      image_ids_notconnected.push_back(image.first);
-      image.second.frame_ptr->is_registered = false;
+  for (auto& [image_id, image] : images) {
+    if (!database_cache->ExistsImage(image_id) && image.HasPose()) {
+      image_ids_notconnected.push_back(image_id);
+      image.FramePtr()->ResetPose();
     }
   }
 
@@ -134,7 +133,8 @@ bool RetriangulateTracks(const TriangulatorOptions& options,
 
   // Add the removed images to the reconstruction
   for (const auto& image_id : image_ids_notconnected) {
-    images[image_id].frame_ptr->is_registered = true;
+    const auto& image = images[image_id];
+    image.FramePtr()->SetRigFromWorld(Rigid3d());
     colmap::Image image_colmap;
     ConvertGlomapToColmapImage(images[image_id], image_colmap, true);
     reconstruction->AddImage(std::move(image_colmap));

src/glomap/controllers/option_manager.cc
@@ -282,6 +282,8 @@ void OptionManager::AddGravityRefinerOptions() {
                               &gravity_refiner->min_num_neighbors);
 }
 void OptionManager::Reset() {
+  FLAGS_logtostderr = true;
+
   const bool kResetPaths = true;
   ResetOptions(kResetPaths);
 

src/glomap/estimators/bundle_adjustment.cc
@@ -125,8 +125,8 @@ void BundleAdjuster::AddPointToCameraConstraints(
       if (images.find(observation.image_id) == images.end()) continue;
 
       Image& image = images[observation.image_id];
-      Frame* frame_ptr = image.frame_ptr;
-      const image_t rig_id = image.frame_ptr->RigId();
+      colmap::Frame* frame = image.FramePtr();
+      const image_t rig_id = frame->RigId();
 
       ceres::CostFunction* cost_function = nullptr;
       // if (image_id_to_camera_rig_index_.find(observation.first) ==
@@ -134,55 +134,55 @@ void BundleAdjuster::AddPointToCameraConstraints(
       if (image.IsRefInFrame()) {
         cost_function =
             colmap::CreateCameraCostFunction<colmap::ReprojErrorCostFunctor>(
-                cameras[image.camera_id].model_id,
+                cameras[image.CameraId()].model_id,
                 image.features[observation.point2D_idx]);
         problem_->AddResidualBlock(
             cost_function,
             loss_function_.get(),
-            frame_ptr->RigFromWorld().rotation.coeffs().data(),
-            frame_ptr->RigFromWorld().translation.data(),
+            frame->RigFromWorld().rotation.coeffs().data(),
+            frame->RigFromWorld().translation.data(),
             tracks[track_id].xyz.data(),
-            cameras[image.camera_id].params.data());
+            cameras[image.CameraId()].params.data());
       } else if (!options_.optimize_rig_poses) {
         const Rigid3d& cam_from_rig = rigs[rig_id].SensorFromRig(
-            sensor_t(SensorType::CAMERA, image.camera_id));
+            sensor_t(SensorType::CAMERA, image.CameraId()));
         cost_function = colmap::CreateCameraCostFunction<
             colmap::RigReprojErrorConstantRigCostFunctor>(
-            cameras[image.camera_id].model_id,
+            cameras[image.CameraId()].model_id,
             image.features[observation.point2D_idx],
             cam_from_rig);
         problem_->AddResidualBlock(
             cost_function,
             loss_function_.get(),
-            frame_ptr->RigFromWorld().rotation.coeffs().data(),
-            frame_ptr->RigFromWorld().translation.data(),
+            frame->RigFromWorld().rotation.coeffs().data(),
+            frame->RigFromWorld().translation.data(),
             tracks[track_id].xyz.data(),
-            cameras[image.camera_id].params.data());
+            cameras[image.CameraId()].params.data());
       } else {
         // If the image is part of a camera rig, use the RigBATA error
         // Down weight the uncalibrated cameras
         Rigid3d& cam_from_rig = rigs[rig_id].SensorFromRig(
-            sensor_t(SensorType::CAMERA, image.camera_id));
+            sensor_t(SensorType::CAMERA, image.CameraId()));
         cost_function =
             colmap::CreateCameraCostFunction<colmap::RigReprojErrorCostFunctor>(
-                cameras[image.camera_id].model_id,
+                cameras[image.CameraId()].model_id,
                 image.features[observation.point2D_idx]);
         problem_->AddResidualBlock(
             cost_function,
             loss_function_.get(),
             cam_from_rig.rotation.coeffs().data(),
             cam_from_rig.translation.data(),
-            frame_ptr->RigFromWorld().rotation.coeffs().data(),
-            frame_ptr->RigFromWorld().translation.data(),
+            frame->RigFromWorld().rotation.coeffs().data(),
+            frame->RigFromWorld().translation.data(),
             tracks[track_id].xyz.data(),
-            cameras[image.camera_id].params.data());
+            cameras[image.CameraId()].params.data());
       }
 
       if (cost_function != nullptr) {
       } else {
         LOG(ERROR) << "Camera model not supported: "
                    << colmap::CameraModelIdToName(
-                          cameras[image.camera_id].model_id);
+                          cameras[image.CameraId()].model_id);
       }
     }
   }

src/glomap/estimators/global_positioning.cc
@@ -129,8 +129,8 @@ void GlobalPositioner::InitializeRandomPositions(
   constrained_positions.reserve(frames.size());
   for (const auto& [pair_id, image_pair] : view_graph.image_pairs) {
     if (image_pair.is_valid == false) continue;
-    constrained_positions.insert(images[image_pair.image_id1].frame_id);
-    constrained_positions.insert(images[image_pair.image_id2].frame_id);
+    constrained_positions.insert(images[image_pair.image_id1].FrameId());
+    constrained_positions.insert(images[image_pair.image_id2].FrameId());
   }
 
   for (const auto& [track_id, track] : tracks) {
@@ -138,8 +138,8 @@ void GlobalPositioner::InitializeRandomPositions(
     for (const auto& observation : tracks[track_id].track.Elements()) {
       if (images.find(observation.image_id) == images.end()) continue;
       Image& image = images[observation.image_id];
-      if (!image.IsRegistered()) continue;
-      constrained_positions.insert(images[observation.image_id].frame_id);
+      if (!image.HasPose()) continue;
+      constrained_positions.insert(images[observation.image_id].FrameId());
     }
   }
 
@@ -173,7 +173,7 @@ void GlobalPositioner::AddCameraToCameraConstraints(
     const ViewGraph& view_graph, std::unordered_map<image_t, Image>& images) {
   // For cam to cam constraint, only support the trivial frames now
   for (const auto& [image_id, image] : images) {
-    if (!image.IsRegistered()) continue;
+    if (!image.HasPose()) continue;
     if (!image.IsRefInFrame()) {
       LOG(ERROR) << "Now, only trivial frames are supported for the camera to "
                     "camera constraints";
@@ -202,8 +202,8 @@ void GlobalPositioner::AddCameraToCameraConstraints(
     problem_->AddResidualBlock(
         cost_function,
         loss_function_.get(),
-        images[image_id1].frame_ptr->RigFromWorld().translation.data(),
-        images[image_id2].frame_ptr->RigFromWorld().translation.data(),
+        images[image_id1].FramePtr()->RigFromWorld().translation.data(),
+        images[image_id2].FramePtr()->RigFromWorld().translation.data(),
         &scale);
 
     problem_->SetParameterLowerBound(&scale, 0, 1e-5);
@@ -288,7 +288,7 @@ void GlobalPositioner::AddTrackToProblem(
     if (images.find(observation.image_id) == images.end()) continue;
 
     Image& image = images[observation.image_id];
-    if (!image.IsRegistered()) continue;
+    if (!image.HasPose()) continue;
 
     const Eigen::Vector3d& feature_undist =
         image.features_undist[observation.point2D_idx];
@@ -320,7 +320,7 @@ void GlobalPositioner::AddTrackToProblem(
     // functions
     // Down weight the uncalibrated cameras
     ceres::LossFunction* loss_function =
-        (cameras[image.camera_id].has_prior_focal_length)
+        (cameras[image.CameraId()].has_prior_focal_length)
             ? loss_function_ptcam_calibrated_.get()
             : loss_function_ptcam_uncalibrated_.get();
 
@@ -332,15 +332,15 @@ void GlobalPositioner::AddTrackToProblem(
       problem_->AddResidualBlock(
           cost_function,
           loss_function,
-          image.frame_ptr->RigFromWorld().translation.data(),
+          image.FramePtr()->RigFromWorld().translation.data(),
           track.xyz.data(),
           &scale);
       // If the image is part of a camera rig, use the RigBATA error
     } else {
-      rig_t rig_id = image.frame_ptr->RigId();
+      rig_t rig_id = image.FramePtr()->RigId();
       // Otherwise, use the camera rig translation from the frame
       Rigid3d& cam_from_rig = rigs.at(rig_id).SensorFromRig(
-          sensor_t(SensorType::CAMERA, image.camera_id));
+          sensor_t(SensorType::CAMERA, image.CameraId()));
 
       Eigen::Vector3d cam_from_rig_translation = cam_from_rig.translation;
 
@@ -354,7 +354,7 @@ void GlobalPositioner::AddTrackToProblem(
         problem_->AddResidualBlock(
             cost_function,
             loss_function,
-            image.frame_ptr->RigFromWorld().translation.data(),
+            image.FramePtr()->RigFromWorld().translation.data(),
             track.xyz.data(),
             &scale,
             &rig_scales_[rig_id]);
@@ -365,13 +365,13 @@ void GlobalPositioner::AddTrackToProblem(
         // global one
         ceres::CostFunction* cost_function =
             RigUnknownBATAPairwiseDirectionError::Create(
-                translation, image.frame_ptr->RigFromWorld().rotation);
+                translation, image.FramePtr()->RigFromWorld().rotation);
 
         problem_->AddResidualBlock(
             cost_function,
             loss_function,
             track.xyz.data(),
-            image.frame_ptr->RigFromWorld().translation.data(),
+            image.FramePtr()->RigFromWorld().translation.data(),
             cam_from_rig.translation.data(),
             &scale);
       }

src/glomap/estimators/global_rotation_averaging.cc
@@ -67,7 +67,7 @@ std::unordered_map<frame_t, const colmap::PosePrior*> ExtractFrameToPosePrior(
     const std::vector<colmap::PosePrior>& pose_priors) {
   std::unordered_map<image_t, frame_t> image_to_frame;
   for (const auto& [image_id, image] : images) {
-    image_to_frame[image_id] = image.frame_id;
+    image_to_frame[image_id] = image.FrameId();
   }
 
   std::unordered_map<frame_t, const colmap::PosePrior*> frame_to_pose_prior;
@@ -149,13 +149,14 @@ void RotationEstimator::InitializeFromMaximumSpanningTree(
   std::unordered_map<image_t, image_t> parents;
   const image_t root =
       MaximumSpanningTree(view_graph, images, parents, WeightType::INLIER_NUM);
+  THROW_CHECK(images.at(root).HasPose());
 
   // Iterate through the tree to initialize the rotation
   // Establish child info
   std::unordered_map<image_t, std::vector<image_t>> children;
   for (const auto& [image_id, image] : images) {
-    if (!image.IsRegistered()) continue;
-    children.insert(std::make_pair(image_id, std::vector<image_t>()));
+    if (!image.HasPose()) continue;
+    children.emplace(image_id, std::vector<image_t>());
   }
   for (auto& [child, parent] : parents) {
     if (root == child) continue;
@@ -165,7 +166,7 @@ void RotationEstimator::InitializeFromMaximumSpanningTree(
   std::queue<image_t> indexes;
   indexes.push(root);
 
-  std::unordered_map<image_t, Rigid3d> cam_from_worlds;
+  std::unordered_map<image_t, Rigid3d> cams_from_world;
   while (!indexes.empty()) {
     image_t curr = indexes.front();
     indexes.pop();
@@ -180,17 +181,17 @@ void RotationEstimator::InitializeFromMaximumSpanningTree(
         colmap::ImagePairToPairId(curr, parents[curr]));
     if (image_pair.image_id1 == curr) {
       // 1_R_w = 2_R_1^T * 2_R_w
-      cam_from_worlds[curr].rotation =
-          (Inverse(image_pair.cam2_from_cam1) * cam_from_worlds[parents[curr]])
+      cams_from_world[curr].rotation =
+          (Inverse(image_pair.cam2_from_cam1) * cams_from_world[parents[curr]])
               .rotation;
     } else {
       // 2_R_w = 2_R_1 * 1_R_w
-      cam_from_worlds[curr].rotation =
-          (image_pair.cam2_from_cam1 * cam_from_worlds[parents[curr]]).rotation;
+      cams_from_world[curr].rotation =
+          (image_pair.cam2_from_cam1 * cams_from_world[parents[curr]]).rotation;
     }
   }
 
-  ConvertRotationsFromImageToRig(cam_from_worlds, images, rigs, frames);
+  ConvertRotationsFromImageToRig(cams_from_world, images, rigs, frames);
 }
 
 // TODO: refine the code
@@ -221,8 +222,8 @@ void RotationEstimator::SetupLinearSystem(
       image_t image_id = data_id.id;
       if (images.find(image_id) == images.end()) continue;
       const auto& image = images.at(image_id);
-      if (!image.IsRegistered()) continue;
-      camera_id_to_rig_id[image.camera_id] = frame.RigId();
+      if (!image.HasPose()) continue;
+      camera_id_to_rig_id[image.CameraId()] = frame.RigId();
     }
   }
 
@@ -249,7 +250,7 @@ void RotationEstimator::SetupLinearSystem(
 
   for (auto& [frame_id, frame] : frames) {
     // Skip the unregistered frames
-    if (!frames.at(frame_id).is_registered) continue;
+    if (!frames.at(frame_id).HasPose()) continue;
     frame_id_to_idx_[frame_id] = num_dof;
     image_t image_id_ref = -1;
     for (auto& data_id : frame.ImageIds()) {
@@ -308,7 +309,7 @@ void RotationEstimator::SetupLinearSystem(
   // If no cameras are set to be fixed, then take the first camera
   if (fixed_image_id_ == -1) {
     for (auto& [frame_id, frame] : frames) {
-      if (!frames.at(frame_id).is_registered) continue;
+      if (!frames.at(frame_id).HasPose()) continue;
 
       fixed_image_id_ = frame.DataIds().begin()->id;
       const Eigen::AngleAxisd rig_from_world(frame.RigFromWorld().rotation);
@@ -326,8 +327,8 @@ void RotationEstimator::SetupLinearSystem(
 
     const auto& image1 = images.at(image_pair.image_id1);
     const auto& image2 = images.at(image_pair.image_id2);
-    const auto& frame1 = frames.at(image1.frame_id);
-    const auto& frame2 = frames.at(image2.frame_id);
+    const auto& frame1 = frames.at(image1.FrameId());
+    const auto& frame2 = frames.at(image2.FrameId());
 
     const int image_idx1 = image_id_to_idx_[image_pair.image_id1];
     const int image_idx2 = image_id_to_idx_[image_pair.image_id2];
@@ -337,18 +338,20 @@ void RotationEstimator::SetupLinearSystem(
     bool has_sensor_from_rig1 = false;
     bool has_sensor_from_rig2 = false;
     if (!image1.IsRefInFrame()) {
-      if (camera_id_to_idx_.find(image1.camera_id) == camera_id_to_idx_.end()) {
+      if (camera_id_to_idx_.find(image1.CameraId()) ==
+          camera_id_to_idx_.end()) {
         cam1_from_rig1 =
             rigs.at(frame1.RigId())
-                .SensorFromRig(sensor_t(SensorType::CAMERA, image1.camera_id));
+                .SensorFromRig(sensor_t(SensorType::CAMERA, image1.CameraId()));
         has_sensor_from_rig1 = true;
       }
     }
     if (!image2.IsRefInFrame()) {
-      if (camera_id_to_idx_.find(image2.camera_id) == camera_id_to_idx_.end()) {
+      if (camera_id_to_idx_.find(image2.CameraId()) ==
+          camera_id_to_idx_.end()) {
         cam2_from_rig2 =
             rigs.at(frame2.RigId())
-                .SensorFromRig(sensor_t(SensorType::CAMERA, image2.camera_id));
+                .SensorFromRig(sensor_t(SensorType::CAMERA, image2.CameraId()));
         has_sensor_from_rig2 = true;
       }
     }
@@ -366,9 +369,9 @@ void RotationEstimator::SetupLinearSystem(
             .toRotationMatrix();
 
     const Eigen::Vector3d* frame_gravity1 =
-        GetFrameGravityOrNull(frame_to_pose_prior, image1.frame_id);
+        GetFrameGravityOrNull(frame_to_pose_prior, image1.FrameId());
     const Eigen::Vector3d* frame_gravity2 =
-        GetFrameGravityOrNull(frame_to_pose_prior, image2.frame_id);
+        GetFrameGravityOrNull(frame_to_pose_prior, image2.FrameId());
     if (options_.use_gravity) {
       if (frame_gravity1 != nullptr) {
         rel_temp_info_[pair_id].R_rel =
@@ -414,23 +417,23 @@ void RotationEstimator::SetupLinearSystem(
 
     const auto& image1 = images.at(image_pair.image_id1);
     const auto& image2 = images.at(image_pair.image_id2);
-    const auto& frame1 = frames.at(image1.frame_id);
-    const auto& frame2 = frames.at(image2.frame_id);
+    const auto& frame1 = frames.at(image1.FrameId());
+    const auto& frame2 = frames.at(image2.FrameId());
 
     const int image_idx1 = image_id_to_idx_[image_pair.image_id1];
     const int image_idx2 = image_id_to_idx_[image_pair.image_id2];
 
-    if (!frame1.is_registered || !frame2.is_registered) {
+    if (!frame1.HasPose() || !frame2.HasPose()) {
       continue;  // skip unregistered frames
     }
 
     int vector_idx_cam1 = -1;
     int vector_idx_cam2 = -1;
-    if (camera_id_to_idx_.find(image1.camera_id) != camera_id_to_idx_.end()) {
-      vector_idx_cam1 = camera_id_to_idx_[image1.camera_id];
+    if (camera_id_to_idx_.find(image1.CameraId()) != camera_id_to_idx_.end()) {
+      vector_idx_cam1 = camera_id_to_idx_[image1.CameraId()];
     }
-    if (camera_id_to_idx_.find(image2.camera_id) != camera_id_to_idx_.end()) {
-      vector_idx_cam2 = camera_id_to_idx_[image2.camera_id];
+    if (camera_id_to_idx_.find(image2.CameraId()) != camera_id_to_idx_.end()) {
+      vector_idx_cam2 = camera_id_to_idx_[image2.CameraId()];
     }
 
     rel_temp_info_[pair_id].index = curr_pos;
@@ -449,7 +452,7 @@ void RotationEstimator::SetupLinearSystem(
     } else {
       // If it is not gravity aligned, then we need to consider 3 dof
       if (!options_.use_gravity ||
-          GetFrameGravityOrNull(frame_to_pose_prior, image1.frame_id) ==
+          GetFrameGravityOrNull(frame_to_pose_prior, image1.FrameId()) ==
               nullptr) {
         for (int i = 0; i < 3; i++) {
           coeffs.emplace_back(
@@ -462,7 +465,7 @@ void RotationEstimator::SetupLinearSystem(
 
       // Similarly for the second componenet
       if (!options_.use_gravity ||
-          GetFrameGravityOrNull(frame_to_pose_prior, image2.frame_id) ==
+          GetFrameGravityOrNull(frame_to_pose_prior, image2.FrameId()) ==
               nullptr) {
         for (int i = 0; i < 3; i++) {
           coeffs.emplace_back(
@@ -503,7 +506,7 @@ void RotationEstimator::SetupLinearSystem(
 
   const Image& fixed_image = images.at(fixed_image_id_);
   const auto fixed_pose_prior_it =
-      frame_to_pose_prior.find(fixed_image.frame_id);
+      frame_to_pose_prior.find(fixed_image.FrameId());
   const bool fixed_image_has_gravity =
       fixed_pose_prior_it != frame_to_pose_prior.end() &&
       fixed_pose_prior_it->second->HasGravity();
@@ -627,7 +630,7 @@ bool RotationEstimator::SolveIRLS(
 
   const Image& fixed_image = images.at(fixed_image_id_);
   const auto fixed_pose_prior_it =
-      frame_to_pose_prior.find(fixed_image.frame_id);
+      frame_to_pose_prior.find(fixed_image.FrameId());
   const bool fixed_image_has_gravity =
       fixed_pose_prior_it != frame_to_pose_prior.end() &&
       fixed_pose_prior_it->second->HasGravity();
@@ -710,7 +713,7 @@ void RotationEstimator::UpdateGlobalRotations(
     const std::unordered_map<frame_t, const colmap::PosePrior*>&
         frame_to_pose_prior) {
   for (const auto& [frame_id, frame] : frames) {
-    if (!frame.is_registered) continue;
+    if (!frame.HasPose()) continue;
     const frame_t frame_idx = frame_id_to_idx_[frame_id];
     const auto pose_prior_it = frame_to_pose_prior.find(frame_id);
     const bool has_gravity = pose_prior_it != frame_to_pose_prior.end() &&
@@ -732,7 +735,7 @@ void RotationEstimator::UpdateGlobalRotations(
     cam_from_rigs[camera_id] = std::vector<Eigen::Matrix3d>();
   }
   for (const auto& [frame_id, frame] : frames) {
-    if (!frames.at(frame_id).is_registered) continue;
+    if (!frames.at(frame_id).HasPose()) continue;
     const auto pose_prior_it = frame_to_pose_prior.find(frame_id);
     const bool has_gravity = pose_prior_it != frame_to_pose_prior.end() &&
                              pose_prior_it->second->HasGravity();
@@ -749,8 +752,8 @@ void RotationEstimator::UpdateGlobalRotations(
     for (const auto& data_id : frame.ImageIds()) {
       if (images.find(data_id.id) == images.end()) continue;
       const auto& image = images.at(data_id.id);
-      if (camera_id_to_idx_.find(image.camera_id) != camera_id_to_idx_.end()) {
-        cam_from_rigs[image.camera_id].push_back(R_ori);
+      if (camera_id_to_idx_.find(image.CameraId()) != camera_id_to_idx_.end()) {
+        cam_from_rigs[image.CameraId()].push_back(R_ori);
       }
     }
   }
@@ -803,9 +806,9 @@ void RotationEstimator::ComputeResiduals(
       const Image& image2 = images.at(image_id2);
 
       const Eigen::Vector3d* frame_gravity1 =
-          GetFrameGravityOrNull(frame_to_pose_prior, image1.frame_id);
+          GetFrameGravityOrNull(frame_to_pose_prior, image1.FrameId());
       const Eigen::Vector3d* frame_gravity2 =
-          GetFrameGravityOrNull(frame_to_pose_prior, image2.frame_id);
+          GetFrameGravityOrNull(frame_to_pose_prior, image2.FrameId());
 
       if (options_.use_gravity && frame_gravity1 != nullptr) {
         R_1 = AngleToRotUp(rotation_estimated_[image_id_to_idx_[image_id1]]);
@@ -842,7 +845,7 @@ void RotationEstimator::ComputeResiduals(
 
   const Image& fixed_image = images.at(fixed_image_id_);
   const auto fixed_pose_prior_it =
-      frame_to_pose_prior.find(fixed_image.frame_id);
+      frame_to_pose_prior.find(fixed_image.FrameId());
   const bool fixed_image_has_gravity =
       fixed_pose_prior_it != frame_to_pose_prior.end() &&
       fixed_pose_prior_it->second->HasGravity();
@@ -866,7 +869,7 @@ double RotationEstimator::ComputeAverageStepSize(
         frame_to_pose_prior) {
   double total_update = 0;
   for (const auto& [frame_id, frame] : frames) {
-    if (!frames.at(frame_id).is_registered) continue;
+    if (frames.at(frame_id).HasPose()) continue;
     const auto pose_prior_it = frame_to_pose_prior.find(frame_id);
     const bool has_gravity = pose_prior_it != frame_to_pose_prior.end() &&
                              pose_prior_it->second->HasGravity();
@@ -886,7 +889,7 @@ void RotationEstimator::ConvertResults(
     const std::unordered_map<frame_t, const colmap::PosePrior*>&
         frame_to_pose_prior) {
   for (auto& [frame_id, frame] : frames) {
-    if (!frames[frame_id].is_registered) continue;
+    if (!frames[frame_id].HasPose()) continue;
 
     const image_t image_id_begin = frame.DataIds().begin()->id;
 

commit 611bf9168071d722e1b0647a244177f01b9c0493

Use DeRegisterFrame over ResetPose in view graph and track retriangulation. (#3877)
Did some regression tests of global pipeline, and found a bug introduced
in https://github.com/colmap/colmap/pull/3805.

From this commit hash the global positioning throws when not all images
are successfully registered. This PR fixes the issue.

src/glomap/scene/view_graph.cc
@@ -82,8 +82,8 @@ int ViewGraph::KeepLargestConnectedComponents(
   const std::unordered_set<frame_t>& largest_component =
       connected_components[max_comp];
   for (const auto& [frame_id, frame] : reconstruction.Frames()) {
-    if (largest_component.count(frame_id) == 0) {
-      reconstruction.Frame(frame_id).ResetPose();
+    if (largest_component.count(frame_id) == 0 && frame.HasPose()) {
+      reconstruction.DeRegisterFrame(frame_id);
     }
   }
 

src/glomap/sfm/track_retriangulation.cc
@@ -49,7 +49,7 @@ bool RetriangulateTracks(const TriangulatorOptions& options,
   for (const auto& [image_id, image] : reconstruction.Images()) {
     if (!database_cache->ExistsImage(image_id) && image.HasPose()) {
       image_ids_notconnected.push_back(image_id);
-      image.FramePtr()->ResetPose();
+      reconstruction.DeRegisterFrame(image.FrameId());
     }
   }
 

src/glomap/scene/view_graph_test.cc
@@ -149,7 +149,7 @@ TEST(ViewGraph, FilterByRelativeRotation) {
   view_graph.image_pairs.emplace(pair_id3, std::move(pair3));
   view_graph.image_pairs.emplace(pair_id4, std::move(pair4));
 
-  reconstruction.Image(id4).FramePtr()->ResetPose();
+  reconstruction.DeRegisterFrame(reconstruction.Image(id4).FrameId());
 
   view_graph.FilterByRelativeRotation(reconstruction, 5.0);
 