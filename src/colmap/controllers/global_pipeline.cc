ea4f27405c514d57fded6889bbb27ddbbc4b4056
Shaohui Liu <b1ueber2y@gmail.com>
2025-12-28 13:56:40 +0100

    Further cleanup of std::cout in the codebase.

--- src/colmap/controllers/global_pipeline.cc
+++ src/colmap/controllers/global_pipeline.cc
@@ -83,8 +83,8 @@ void GlobalPipeline::Run() {
     }
   } else {
     for (int comp = 0; comp <= max_cluster_id; comp++) {
-      std::cout << "\r Exporting reconstruction " << comp + 1 << " / "
-                << max_cluster_id + 1 << std::flush;
+      LOG(INFO) << "Exporting reconstruction " << comp + 1 << " / "
+                << max_cluster_id + 1;
       Reconstruction& output_reconstruction =
           *reconstruction_manager_->Get(reconstruction_manager_->Add());
       output_reconstruction = glomap::SubReconstructionByClusterId(
@@ -93,7 +93,6 @@ void GlobalPipeline::Run() {
         output_reconstruction.ExtractColorsForAllImages(options_.image_path);
       }
     }
-    std::cout << '\n';
   }
 }
 
