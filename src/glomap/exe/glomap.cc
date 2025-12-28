27b0ff97bef20830c094076fdc624fd3c72f5b59
Shaohui Liu <b1ueber2y@gmail.com>
2025-12-28 18:46:59 +0100

    merge glomap exe to a single file.

--- src/glomap/exe/glomap.cc
+++ src/glomap/exe/glomap.cc
@@ -1,3 +1,35 @@
+// Copyright (c), ETH Zurich and UNC Chapel Hill.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//
+//     * Neither the name of ETH Zurich and UNC Chapel Hill nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "glomap/exe/glomap.h"
+
+#include "colmap/controllers/global_pipeline.h"
 #include "colmap/geometry/pose.h"
 #include "colmap/scene/reconstruction_io_utils.h"
 #include "colmap/util/file.h"
@@ -8,8 +40,96 @@
 #include "glomap/estimators/rotation_averaging.h"
 #include "glomap/io/colmap_io.h"
 #include "glomap/io/pose_io.h"
+#include "glomap/sfm/global_mapper.h"
 
 namespace glomap {
+
+// -------------------------------------
+// Mappers starting from COLMAP database
+// -------------------------------------
+int RunGlobalMapper(int argc, char** argv) {
+  std::string output_path;
+
+  std::string constraint_type = "ONLY_POINTS";
+  std::string output_format = "bin";
+
+  OptionManager options;
+  options.AddDatabaseOptions();
+  options.AddImageOptions();
+  options.AddRequiredOption("output_path", &output_path);
+  options.AddDefaultOption("constraint_type",
+                           &constraint_type,
+                           "{ONLY_POINTS, ONLY_CAMERAS, "
+                           "POINTS_AND_CAMERAS_BALANCED, POINTS_AND_CAMERAS}");
+  options.AddDefaultOption("output_format", &output_format, "{bin, txt}");
+  options.AddGlobalMapperOptions();
+
+  if (!options.Parse(argc, argv)) {
+    return EXIT_FAILURE;
+  }
+
+  if (constraint_type == "ONLY_POINTS") {
+    options.mapper->opt_gp.constraint_type =
+        GlobalPositionerOptions::ONLY_POINTS;
+  } else if (constraint_type == "ONLY_CAMERAS") {
+    options.mapper->opt_gp.constraint_type =
+        GlobalPositionerOptions::ONLY_CAMERAS;
+  } else if (constraint_type == "POINTS_AND_CAMERAS_BALANCED") {
+    options.mapper->opt_gp.constraint_type =
+        GlobalPositionerOptions::POINTS_AND_CAMERAS_BALANCED;
+  } else if (constraint_type == "POINTS_AND_CAMERAS") {
+    options.mapper->opt_gp.constraint_type =
+        GlobalPositionerOptions::POINTS_AND_CAMERAS;
+  } else {
+    LOG(ERROR) << "Invalid constriant type";
+    return EXIT_FAILURE;
+  }
+
+  // Check whether output_format is valid
+  if (output_format != "bin" && output_format != "txt") {
+    LOG(ERROR) << "Invalid output format";
+    return EXIT_FAILURE;
+  }
+
+  auto database = colmap::Database::Open(*options.database_path);
+
+  colmap::Reconstruction reconstruction;
+  ViewGraph view_graph;
+  InitializeGlomapFromDatabase(*database, reconstruction, view_graph);
+
+  std::vector<colmap::PosePrior> pose_priors = database->ReadAllPosePriors();
+
+  if (view_graph.image_pairs.empty()) {
+    LOG(ERROR) << "Can't continue without image pairs";
+    return EXIT_FAILURE;
+  }
+
+  options.mapper->image_path = *options.image_path;
+
+  GlobalMapper global_mapper(*options.mapper);
+
+  // Main solver
+  LOG(INFO) << "Loaded database";
+  colmap::Timer run_timer;
+  run_timer.Start();
+  std::unordered_map<frame_t, int> cluster_ids;
+  global_mapper.Solve(
+      database.get(), view_graph, reconstruction, pose_priors, cluster_ids);
+  run_timer.Pause();
+
+  LOG(INFO) << "Reconstruction done in " << run_timer.ElapsedSeconds()
+            << " seconds";
+
+  WriteReconstructionsByClusters(output_path,
+                                 reconstruction,
+                                 cluster_ids,
+                                 output_format,
+                                 *options.image_path);
+  LOG(INFO) << "Export to COLMAP reconstruction done";
+
+  return EXIT_SUCCESS;
+}
+
 // -------------------------------------
 // Running Global Rotation Averager
 // -------------------------------------
