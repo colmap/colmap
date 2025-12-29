458d58885e795c9ac53fb0b32ee30dca9397bf33
Shaohui Liu <b1ueber2y@gmail.com>
2025-12-29 22:22:46 +0100

    format.

--- src/glomap/io/colmap_io.h
+++ src/glomap/io/colmap_io.h
@@ -15,9 +15,10 @@ void InitializeEmptyReconstructionFromDatabase(
 
 // Initialize the view graph from the database.
 // The reconstruction must already be initialized with images.
-void InitializeViewGraphFromDatabase(const colmap::Database& database,
-                                     const colmap::Reconstruction& reconstruction,
-                                     ViewGraph& view_graph);
+void InitializeViewGraphFromDatabase(
+    const colmap::Database& database,
+    const colmap::Reconstruction& reconstruction,
+    ViewGraph& view_graph);
 
 // Extract a subset of the reconstruction for a specific cluster.
 // Returns a new Reconstruction containing only frames/images/points from the
