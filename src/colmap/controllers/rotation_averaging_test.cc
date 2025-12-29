b00623ca4a506b7f0d66ebbdf64846c6081cddc4
Shaohui Liu <b1ueber2y@gmail.com>
2025-12-29 23:19:18 +0100

    add deterministic test for rotation averaging controller.

--- src/colmap/controllers/rotation_averaging_test.cc
+++ src/colmap/controllers/rotation_averaging_test.cc
@@ -115,5 +115,68 @@ TEST(RotationAveragingController, WithNoiseAndOutliers) {
                        /*max_rotation_error_deg=*/3);
 }
 
+void ExpectExactEqualRotations(const Reconstruction& reconstruction1,
+                               const Reconstruction& reconstruction2) {
+  const std::vector<image_t> reg_image_ids = reconstruction1.RegImageIds();
+  ASSERT_EQ(reg_image_ids.size(), reconstruction2.RegImageIds().size());
+  for (const image_t image_id : reg_image_ids) {
+    EXPECT_EQ(reconstruction1.Image(image_id).CamFromWorld().rotation.coeffs(),
+              reconstruction2.Image(image_id).CamFromWorld().rotation.coeffs());
+  }
+}
+
+TEST(RotationAveragingController, WithRandomSeedStability) {
+  SetPRNGSeed(1);
+
+  const std::string database_path = CreateTestDir() + "/database.db";
+
+  auto database = Database::Open(database_path);
+  Reconstruction gt_reconstruction;
+  SyntheticDatasetOptions synthetic_dataset_options;
+  synthetic_dataset_options.num_rigs = 2;
+  synthetic_dataset_options.num_cameras_per_rig = 1;
+  synthetic_dataset_options.num_frames_per_rig = 5;
+  synthetic_dataset_options.num_points3D = 100;
+  SynthesizeDataset(
+      synthetic_dataset_options, &gt_reconstruction, database.get());
+  SyntheticNoiseOptions synthetic_noise_options;
+  synthetic_noise_options.point2D_stddev = 0.5;
+  SynthesizeNoise(synthetic_noise_options, &gt_reconstruction, database.get());
+
+  auto run_controller = [&](int num_threads, int random_seed) {
+    auto reconstruction = std::make_shared<Reconstruction>();
+    RotationAveragingControllerOptions options;
+    options.num_threads = num_threads;
+    options.random_seed = random_seed;
+    RotationAveragingController controller(options, database, reconstruction);
+    controller.Run();
+    return reconstruction;
+  };
+
+  constexpr int kRandomSeed = 42;
+
+  // Single-threaded execution.
+  {
+    auto reconstruction0 =
+        run_controller(/*num_threads=*/1, /*random_seed=*/kRandomSeed);
+    auto reconstruction1 =
+        run_controller(/*num_threads=*/1, /*random_seed=*/kRandomSeed);
+    ExpectExactEqualRotations(*reconstruction0, *reconstruction1);
+  }
+
+  // Multi-threaded execution.
+  {
+    auto reconstruction0 =
+        run_controller(/*num_threads=*/3, /*random_seed=*/kRandomSeed);
+    auto reconstruction1 =
+        run_controller(/*num_threads=*/3, /*random_seed=*/kRandomSeed);
+    // Same seed should produce similar results, up to floating-point variations
+    // in optimization.
+    ExpectEqualRotations(*reconstruction0,
+                         *reconstruction1,
+                         /*max_rotation_error_deg=*/1e-10);
+  }
+}
+
 }  // namespace
 }  // namespace colmap
