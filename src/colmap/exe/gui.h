09f58675e8e4ec2e7851d1eff7de31c168679ec2
Johannes Schönberger <jsch@meta.com>
2026-01-04 18:11:10 +0100

    d

--- src/colmap/exe/gui.h
+++ src/colmap/exe/gui.h
@@ -27,6 +27,8 @@
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#include <stdexcept>
+
 #if defined(COLMAP_GUI_ENABLED)
 #include <QApplication>
 #else
