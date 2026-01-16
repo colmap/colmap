018bd821ffb7f9f6b4605d0de2a3da7a2e17567e
Shaohui Liu <b1ueber2y@gmail.com>
2026-01-16 12:24:54 +0100

    Move small bundle adjustment runner into global mapper impl.

--- src/glomap/sfm/global_mapper.cc
+++ src/glomap/sfm/global_mapper.cc
@@ -8,13 +8,47 @@
 #include "colmap/util/misc.h"
 #include "colmap/util/timer.h"
 
-#include "glomap/estimators/bundle_adjustment.h"
 #include "glomap/estimators/rotation_averaging.h"
 
 #include <algorithm>
 
 namespace glomap {
 namespace {
+
+bool RunBundleAdjustment(const colmap::BundleAdjustmentOptions& options,
+                         colmap::Reconstruction& reconstruction) {
+  if (reconstruction.NumImages() == 0) {
+    LOG(ERROR) << "Number of images = " << reconstruction.NumImages();
+    return false;
+  }
+  if (reconstruction.NumPoints3D() == 0) {
+    LOG(ERROR) << "Number of tracks = " << reconstruction.NumPoints3D();
+    return false;
+  }
+
+  // Set up bundle adjustment config.
+  colmap::BundleAdjustmentConfig ba_config;
+  for (const auto& [image_id, image] : reconstruction.Images()) {
+    if (image.HasPose()) {
+      ba_config.AddImage(image_id);
+    }
+  }
+  // Use TWO_CAMS_FROM_WORLD for deterministic gauge fixing.
+  ba_config.FixGauge(colmap::BundleAdjustmentGauge::TWO_CAMS_FROM_WORLD);
+
+  auto ba =
+      colmap::CreateDefaultBundleAdjuster(options, ba_config, reconstruction);
+
+  ceres::Solver::Summary summary = ba->Solve();
+
+  if (VLOG_IS_ON(2)) {
+    LOG(INFO) << summary.FullReport();
+  } else {
+    LOG(INFO) << summary.BriefReport();
+  }
+
+  return summary.IsSolutionUsable();
+}
 
 GlobalMapperOptions InitializeOptions(const GlobalMapperOptions& options) {
   // Propagate random seed and num_threads to component options.
