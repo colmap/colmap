09b5ab9e5de71ea7cf6f19b65e1af172cb7340ee
Linfei Pan <linpan@student.ethz.ch>
2026-01-21 18:33:16 +0100

    misc

--- src/colmap/scene/reconstruction_clustering.cc
+++ src/colmap/scene/reconstruction_clustering.cc
@@ -102,7 +102,7 @@ std::unordered_map<frame_t, int> EstablishStrongClusters(
 
   // Ensure all nodes are assigned a cluster ID.
   for (const auto node : nodes) {
-    if (cluster_ids.find(node) == cluster_ids.end()) cluster_ids[node] = -1;
+    if (!uf.FindIfExists(node).has_value()) cluster_ids[node] = -1;
   }
 
   LOG(INFO) << "Frames are grouped into " << num_valid_clusters
