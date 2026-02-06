a49770a776638702f8ea69080d2f7f8aea1fdd78
Johannes Schönberger <jsch@meta.com>
2026-02-06 20:27:23 +0100

    d

--- src/pycolmap/feature/types.h
+++ src/pycolmap/feature/types.h
@@ -1,18 +1,9 @@
 #pragma once
 
-#include "colmap/feature/types.h"
-
-#include <pybind11/pybind11.h>
-
-// PYBIND11_MAKE_OPAQUE must be declared before including pybind11/stl.h
-// to avoid ODR violations with the default type_caster.
-PYBIND11_MAKE_OPAQUE(colmap::FeatureKeypoints);
-PYBIND11_MAKE_OPAQUE(colmap::FeatureMatches);
-
+#include "pycolmap/feature/opaque_types.h"
 #include "pycolmap/helpers.h"
 #include "pycolmap/pybind11_extension.h"
 
 #include <pybind11/eigen.h>
-#include <pybind11/stl.h>
 
 namespace py = pybind11;
