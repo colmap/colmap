1a4617bd89e178d7dbf5fa351732c76bd3ebcda3
Johannes Schönberger <jsch@meta.com>
2026-02-06 20:05:02 +0100

    d

--- python/generate_stubs.sh
+++ python/generate_stubs.sh
@@ -26,8 +26,5 @@ perl -i -pe's/self: (typing|collections\.abc)\.Sequence\[[a-zA-Z0-9_.]+\]/self/g
 # mypy bug: https://github.com/python/mypy/issues/4266
 perl -i -pe's/(__hash__:? .*= None)$/\1  # type: ignore/g' $FILES
 
-# pybind issue: dictionary keys should not be cast to the more generic types.
-perl -i -pe's/Mapping\[typing.Supports(Int|Float)/Mapping\[\L\1/g' $FILES
-
 COLMAP_DIR=$(dirname $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ))
 ruff format --config ${COLMAP_DIR}/ruff.toml ${FILES}
