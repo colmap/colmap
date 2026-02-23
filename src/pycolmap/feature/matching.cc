706d9ed356d28d9946e84e949ad6116ce01e6d86
Johannes Schönberger <jsch@meta.com>
2026-02-23 21:30:28 +0100

    Add Python bindings for feature matcher

--- src/pycolmap/feature/matching.cc
+++ src/pycolmap/feature/matching.cc
@@ -1,5 +1,10 @@
+#include "colmap/feature/matcher.h"
 #include "colmap/feature/sift.h"
 #include "colmap/feature/utils.h"
+#ifdef COLMAP_ONNX_ENABLED
+#include "colmap/feature/aliked.h"
+#include "colmap/feature/onnx_matchers.h"
+#endif
 
 #include "pycolmap/feature/types.h"
 #include "pycolmap/helpers.h"
@@ -8,12 +13,97 @@
 #include <Eigen/Core>
 #include <pybind11/eigen.h>
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 using namespace colmap;
 using namespace pybind11::literals;
 namespace py = pybind11;
 
+namespace {
+
+class PyFeatureMatcher : public FeatureMatcher,
+                         py::trampoline_self_life_support {
+ public:
+  using MatcherImage = FeatureMatcher::Image;
+
+  void Match(const MatcherImage& image1,
+             const MatcherImage& image2,
+             FeatureMatches* matches) override {
+    PYBIND11_OVERRIDE_PURE(
+        void, FeatureMatcher, Match, image1, image2, matches);
+  }
+
+  void MatchGuided(double max_error,
+                   const MatcherImage& image1,
+                   const MatcherImage& image2,
+                   TwoViewGeometry* two_view_geometry) override {
+    PYBIND11_OVERRIDE_PURE(void,
+                           FeatureMatcher,
+                           MatchGuided,
+                           max_error,
+                           image1,
+                           image2,
+                           two_view_geometry);
+  }
+};
+
+}  // namespace
+
 void BindFeatureMatching(py::module& m) {
+  py::enum_<FeatureMatcherType>(m, "FeatureMatcherType")
+      .value("UNDEFINED", FeatureMatcherType::UNDEFINED)
+      .value("SIFT_BRUTEFORCE", FeatureMatcherType::SIFT_BRUTEFORCE)
+      .value("SIFT_LIGHTGLUE", FeatureMatcherType::SIFT_LIGHTGLUE)
+      .value("ALIKED_BRUTEFORCE", FeatureMatcherType::ALIKED_BRUTEFORCE)
+      .value("ALIKED_LIGHTGLUE", FeatureMatcherType::ALIKED_LIGHTGLUE);
+
+#ifdef COLMAP_ONNX_ENABLED
+  auto PyBruteForceONNXMatchingOptions =
+      py::classh<BruteForceONNXMatchingOptions>(m,
+                                                "BruteForceONNXMatchingOptions")
+          .def(py::init<>())
+          .def_readwrite("min_cossim",
+                         &BruteForceONNXMatchingOptions::min_cossim,
+                         "Minimum cosine similarity for a match to be "
+                         "considered valid.")
+          .def_readwrite("max_ratio",
+                         &BruteForceONNXMatchingOptions::max_ratio,
+                         "Maximum ratio for Lowe's ratio test.")
+          .def_readwrite("cross_check",
+                         &BruteForceONNXMatchingOptions::cross_check,
+                         "Enable cross-checking (mutual nearest neighbor).")
+          .def_readwrite("model_path",
+                         &BruteForceONNXMatchingOptions::model_path,
+                         "Path to the ONNX model file.")
+          .def("check", &BruteForceONNXMatchingOptions::Check);
+  MakeDataclass(PyBruteForceONNXMatchingOptions);
+
+  auto PyLightGlueONNXMatchingOptions =
+      py::classh<LightGlueONNXMatchingOptions>(m,
+                                               "LightGlueONNXMatchingOptions")
+          .def(py::init<>())
+          .def_readwrite("min_score",
+                         &LightGlueONNXMatchingOptions::min_score,
+                         "Minimum match score threshold.")
+          .def_readwrite("model_path",
+                         &LightGlueONNXMatchingOptions::model_path,
+                         "Path to the LightGlue ONNX model file.")
+          .def("check", &LightGlueONNXMatchingOptions::Check);
+  MakeDataclass(PyLightGlueONNXMatchingOptions);
+
+  auto PyAlikedMatchingOptions =
+      py::classh<AlikedMatchingOptions>(m, "AlikedMatchingOptions")
+          .def(py::init<>())
+          .def_readwrite("brute_force",
+                         &AlikedMatchingOptions::brute_force,
+                         "Brute-force matching options.")
+          .def_readwrite("lightglue",
+                         &AlikedMatchingOptions::lightglue,
+                         "LightGlue matching options.")
+          .def("check", &AlikedMatchingOptions::Check);
+  MakeDataclass(PyAlikedMatchingOptions);
+#endif
+
   auto PySiftMatchingOptions =
       py::classh<SiftMatchingOptions>(m, "SiftMatchingOptions")
           .def(py::init<>())
@@ -31,12 +121,19 @@ void BindFeatureMatching(py::module& m) {
               "cpu_brute_force_matcher",
               &SiftMatchingOptions::cpu_brute_force_matcher,
               "Whether to use brute-force instead of faiss based CPU matching.")
+#ifdef COLMAP_ONNX_ENABLED
+          .def_readwrite("lightglue",
+                         &SiftMatchingOptions::lightglue,
+                         "LightGlue matching options.")
+#endif
           .def("check", &SiftMatchingOptions::Check);
   MakeDataclass(PySiftMatchingOptions);
 
   auto PyFeatureMatchingOptions =
       py::classh<FeatureMatchingOptions>(m, "FeatureMatchingOptions")
-          .def(py::init<>())
+          .def(py::init<FeatureMatcherType>(),
+               "type"_a = FeatureMatcherType::SIFT_BRUTEFORCE)
+          .def_readwrite("type", &FeatureMatchingOptions::type)
           .def_readwrite("num_threads", &FeatureMatchingOptions::num_threads)
           .def_readwrite("use_gpu", &FeatureMatchingOptions::use_gpu)
           .def_readwrite("gpu_index",
@@ -73,5 +170,63 @@ void BindFeatureMatching(py::module& m) {
               "useful for the case of non-overlapping cameras in a rig.")
           .def_readwrite("sift", &FeatureMatchingOptions::sift)
           .def("check", &FeatureMatchingOptions::Check);
+#ifdef COLMAP_ONNX_ENABLED
+  PyFeatureMatchingOptions.def_readwrite("aliked",
+                                         &FeatureMatchingOptions::aliked);
+#endif
   MakeDataclass(PyFeatureMatchingOptions);
+
+  auto PyFeatureMatcherCls =
+      py::classh<FeatureMatcher, PyFeatureMatcher>(m, "FeatureMatcher");
+
+  py::classh<FeatureMatcher::Image>(PyFeatureMatcherCls, "Image")
+      .def(py::init<>())
+      .def(py::init([](image_t image_id,
+                       const Camera* camera,
+                       std::shared_ptr<const FeatureKeypoints> keypoints,
+                       std::shared_ptr<const FeatureDescriptors> descriptors) {
+             FeatureMatcher::Image image;
+             image.image_id = image_id;
+             image.camera = camera;
+             image.keypoints = std::move(keypoints);
+             image.descriptors = std::move(descriptors);
+             return image;
+           }),
+           "image_id"_a = kInvalidImageId,
+           "camera"_a = nullptr,
+           "keypoints"_a = nullptr,
+           "descriptors"_a = nullptr)
+      .def_readwrite("image_id", &FeatureMatcher::Image::image_id)
+      .def_readwrite("camera", &FeatureMatcher::Image::camera)
+      .def_readwrite("keypoints", &FeatureMatcher::Image::keypoints)
+      .def_readwrite("descriptors", &FeatureMatcher::Image::descriptors);
+
+  PyFeatureMatcherCls.def_static("create", &FeatureMatcher::Create, "options"_a)
+      .def(
+          "match",
+          [](FeatureMatcher& self,
+             const FeatureMatcher::Image& image1,
+             const FeatureMatcher::Image& image2) {
+            FeatureMatches matches;
+            self.Match(image1, image2, &matches);
+            return matches;
+          },
+          "image1"_a,
+          "image2"_a,
+          "Match features between two images. Returns FeatureMatches.")
+      .def(
+          "match_guided",
+          [](FeatureMatcher& self,
+             double max_error,
+             const FeatureMatcher::Image& image1,
+             const FeatureMatcher::Image& image2,
+             TwoViewGeometry& two_view_geometry) {
+            self.MatchGuided(max_error, image1, image2, &two_view_geometry);
+          },
+          "max_error"_a,
+          "image1"_a,
+          "image2"_a,
+          "two_view_geometry"_a,
+          "Perform guided matching using existing two-view geometry. "
+          "Updates the two_view_geometry in-place.");
 }
