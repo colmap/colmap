8630e070035c21d095cf7dc532ea624cf907da4f
Johannes Schönberger <jsch@meta.com>
2026-02-24 18:25:43 +0100

    d

--- src/pycolmap/feature/bindings.cc
+++ src/pycolmap/feature/bindings.cc
@@ -2,12 +2,10 @@
 
 namespace py = pybind11;
 
-void BindFeatureTypes(py::module& m);
 void BindFeatureExtraction(py::module& m);
 void BindFeatureMatching(py::module& m);
 
 void BindFeature(py::module& m) {
-  BindFeatureTypes(m);
   BindFeatureExtraction(m);
   BindFeatureMatching(m);
 }
