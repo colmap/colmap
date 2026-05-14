09810d3b116e00398c8c77edfdba451f51844dce
StonerLing <stn.suhp@outlook.com>
2026-05-14 16:35:27 +0800

    Fix MSVC build of Caspar

---
+++ src/thirdparty/Symforce-Caspar/msvc_compact.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+#ifdef _MSC_VER
+// Caspar uses 'uint' which is not defined in MSVC.
+using uint = unsigned int;
+
+// solver.cc uses std::to_string but misses the <string> header.
+#include <string>
+#endif
