colmap/colmap

commit 1625d9e8c97aa8beef0df0c00430c1c3d79190ab

Support both Qt5 and Qt6 (#3597)
Co-authored-by: Johannes Schönberger <jsch@meta.com>
Co-authored-by: Johannes Schönberger <jsch@demuc.de>

cmake/FindDependencies.cmake
@@ -198,11 +198,19 @@ else()
 endif()
 
 if(GUI_ENABLED)
-    find_package(Qt5 5.4 ${COLMAP_FIND_TYPE} COMPONENTS Core OpenGL Widgets)
+    find_package(QT NAMES Qt5 Qt6 REQUIRED)
+    set(COLMAP_QT_COMPONENTS Core OpenGL Widgets)
+    if(${QT_VERSION_MAJOR} GREATER_EQUAL 6)
+        list(APPEND COLMAP_QT_COMPONENTS OpenGLWidgets)
+    endif()
+    find_package(Qt${QT_VERSION_MAJOR} ${COLMAP_FIND_TYPE} ${COLMAP_QT_COMPONENTS})
     message(STATUS "Found Qt")
-    message(STATUS "  Module : ${Qt5Core_DIR}")
-    message(STATUS "  Module : ${Qt5OpenGL_DIR}")
-    message(STATUS "  Module : ${Qt5Widgets_DIR}")
+    message(STATUS "  Module : ${Qt${QT_VERSION_MAJOR}Core_DIR}")
+    message(STATUS "  Module : ${Qt${QT_VERSION_MAJOR}OpenGL_DIR}")
+    message(STATUS "  Module : ${Qt${QT_VERSION_MAJOR}Widgets_DIR}")
+    if(${QT_VERSION_MAJOR} GREATER_EQUAL 6)
+        message(STATUS "  Module : ${Qt${QT_VERSION_MAJOR}OpenGLWidgets_DIR}")
+    endif()
     if(Qt5_FOUND)
         # Qt5 was built with -reduce-relocations.
         if(Qt5_POSITION_INDEPENDENT_CODE)
@@ -218,13 +226,15 @@ if(GUI_ENABLED)
                 endif()
             endif()
         endif()
+    endif()
 
+    if(QT_FOUND)
         # Enable automatic compilation of Qt resource files.
         set(CMAKE_AUTORCC ON)
     endif()
 endif()
 
-if(GUI_ENABLED AND Qt5_FOUND)
+if(GUI_ENABLED AND Qt${QT_VERSION_MAJOR}_FOUND)
     list(APPEND COLMAP_COMPILE_DEFINITIONS COLMAP_GUI_ENABLED)
     message(STATUS "Enabling GUI support")
 else()

.github/workflows/build-mac.yml
@@ -62,7 +62,7 @@ jobs:
             glog \
             googletest \
             ceres-solver \
-            qt5 \
+            qt \
             glew \
             cgal \
             sqlite3 \
@@ -72,7 +72,7 @@ jobs:
 
       - name: Configure and build
         run: |
-          export PATH="/usr/local/opt/qt@5/bin:$PATH"
+          export PATH="/usr/local/opt/qt/bin:$PATH"
           cmake --version
           mkdir build
           cd build
@@ -80,7 +80,7 @@ jobs:
             -GNinja \
             -DCMAKE_BUILD_TYPE=${{ matrix.config.cmakeBuildType }} \
             -DTESTS_ENABLED=ON \
-            -DQt5_DIR="$(brew --prefix qt@5)/lib/cmake/Qt5"
+            -DQt6_DIR="$(brew --prefix qt)/lib/cmake/Qt6"
           ninja
 
       - name: Run tests

.github/workflows/build-ubuntu.yml
@@ -23,6 +23,7 @@ jobs:
         config: [
           {
             os: ubuntu-24.04,
+            qtVersion: 6,
             cmakeBuildType: RelWithDebInfo,
             asanEnabled: false,
             guiEnabled: true,
@@ -33,6 +34,7 @@ jobs:
           },
           {
             os: ubuntu-22.04,
+            qtVersion: 6,
             cmakeBuildType: Release,
             asanEnabled: false,
             guiEnabled: true,
@@ -43,6 +45,7 @@ jobs:
           },
           {
             os: ubuntu-22.04,
+            qtVersion: 5,
             cmakeBuildType: Release,
             asanEnabled: false,
             guiEnabled: false,
@@ -53,6 +56,7 @@ jobs:
           },
           {
             os: ubuntu-24.04,
+            qtVersion: 6,
             cmakeBuildType: Release,
             asanEnabled: true,
             guiEnabled: false,
@@ -63,6 +67,7 @@ jobs:
           },
           {
             os: ubuntu-24.04,
+            qtVersion: 6,
             cmakeBuildType: ClangTidy,
             asanEnabled: false,
             guiEnabled: false,
@@ -125,6 +130,12 @@ jobs:
 
       - name: Setup Ubuntu
         run: |
+          if [ "${{ matrix.config.qtVersion }}" == "5" ]; then
+            qt_packages="qtbase5-dev libqt5opengl5-dev libcgal-qt5-dev"
+          elif [ "${{ matrix.config.qtVersion }}" == "6" ]; then
+            qt_packages="qt6-base-dev libqt6opengl6-dev libqt6openglwidgets6"
+          fi
+
           sudo apt-get update && sudo apt-get install -y \
             build-essential \
             cmake \
@@ -141,10 +152,8 @@ jobs:
             libgmock-dev \
             libsqlite3-dev \
             libglew-dev \
-            qtbase5-dev \
-            libqt5opengl5-dev \
+            $qt_packages \
             libcgal-dev \
-            libcgal-qt5-dev \
             libgl1-mesa-dri \
             libunwind-dev \
             libcurl4-openssl-dev \

.github/workflows/build-windows.yml
@@ -51,7 +51,7 @@ jobs:
 
     steps:
       - uses: actions/checkout@v4
-      
+
       # We define the vcpkg binary sources using separate variables for read and
       # write operations:
       # * Read sources are defined as inline. These can be read by anyone and,
@@ -82,7 +82,7 @@ jobs:
           key: v${{ env.COMPILER_CACHE_VERSION }}-${{ matrix.config.os }}-${{ matrix.config.cmakeBuildType }}-${{ matrix.config.asanEnabled }}--${{ matrix.config.cudaEnabled }}-${{ github.run_id }}-${{ github.run_number }}
           restore-keys: v${{ env.COMPILER_CACHE_VERSION }}-${{ matrix.config.os }}-${{ matrix.config.cmakeBuildType }}-${{ matrix.config.asanEnabled }}--${{ matrix.config.cudaEnabled }}
           path: ${{ env.COMPILER_CACHE_DIR }}
-      
+
       - name: Install ccache
         shell: pwsh
         run: |
@@ -166,7 +166,7 @@ jobs:
           ../vcpkg/vcpkg.exe export --raw --output-dir vcpkg_export --output colmap
           cp vcpkg_export/colmap/installed/x64-windows/bin/*.dll install/bin
           cp vcpkg_export/colmap/installed/x64-windows-release/bin/*.dll install/bin
-          cp -r vcpkg_export/colmap/installed/x64-windows/plugins install
+          cp -r vcpkg_export/colmap/installed/x64-windows/Qt6/plugins install
           if ($${{ matrix.config.cudaEnabled }}) {
               cp "${{ steps.cuda-toolkit.outputs.CUDA_PATH }}/bin/cudart64_*.dll" install/bin
               cp "${{ steps.cuda-toolkit.outputs.CUDA_PATH }}/bin/curand64_*.dll" install/bin

docker/Dockerfile
@@ -31,8 +31,9 @@ RUN apt-get update && \
         libgmock-dev \
         libsqlite3-dev \
         libglew-dev \
-        qtbase5-dev \
-        libqt5opengl5-dev \
+        qt6-base-dev \
+        libqt6opengl6-dev \
+        libqt6openglwidgets6 \
         libcgal-dev \
         libceres-dev \
         libcurl4-openssl-dev \
@@ -73,9 +74,10 @@ RUN apt-get update && \
         libgl1 \
         libglew2.2 \
         libgoogle-glog0v6t64 \
-        libqt5core5a \
-        libqt5gui5 \
-        libqt5widgets5 \
+        libqt6core6 \
+        libqt6gui6 \
+        libqt6widgets6 \
+        libqt6openglwidgets6 \
         libcurl4 \
         libmkl-locale \
         libmkl-intel-lp64 \

scripts/shell/build_mac_app.sh
@@ -66,7 +66,11 @@ install_name_tool -change @rpath/libtbb.dylib /usr/local/lib/libtbb.dylib $BASE_
 install_name_tool -change @rpath/libtbbmalloc.dylib /usr/local/lib/libtbbmalloc.dylib $BASE_PATH/COLMAP.app/Contents/MacOS/COLMAP
 
 echo "Linking dynamic libraries"
-/usr/local/opt/qt5/bin/macdeployqt "$BASE_PATH/COLMAP.app"
+if [ -d "$(brew --prefix)/opt/qt6" ]; then
+    $(brew --prefix)/opt/qt6/bin/macdeployqt "$BASE_PATH/COLMAP.app"
+else
+    $(brew --prefix)/opt/qt5/bin/macdeployqt "$BASE_PATH/COLMAP.app"
+fi
 
 echo "Wrapping binary"
 cat <<EOM >"$BASE_PATH/COLMAP.app/Contents/MacOS/colmap_gui.sh"

src/colmap/exe/gui.cc
@@ -53,12 +53,14 @@ int RunGraphicalUserInterface(int argc, char** argv) {
     options.Parse(argc, argv);
   }
 
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
-  QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-  QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-#endif
-
   QApplication app(argc, argv);
+
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)) && \
+    (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
+  app.setAttribute(Qt::AA_EnableHighDpiScaling);
+  app.setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+  app.setAttribute(Qt::AA_DontShowIconsInMenus, false);
 
   colmap::MainWindow main_window(options);
   main_window.show();

src/colmap/feature/CMakeLists.txt
@@ -101,5 +101,5 @@ COLMAP_ADD_TEST(
         colmap_util
 )
 if(TESTS_ENABLED AND GUI_ENABLED)
-    target_link_libraries(colmap_feature_sift_test Qt5::Widgets)
+    target_link_libraries(colmap_feature_sift_test Qt${QT_VERSION_MAJOR}::Widgets)
 endif()

commit dec2ec4b4daa53f51cbe0d25edb28bcd2c164718

fix CMake missing COMPONENTS keyword in find_package Qt (#4413)
same as https://github.com/colmap/colmap/pull/4412 but to the main
branch.
Better merge this one and cherry-pick it to the release branches.

Commit
https://github.com/colmap/colmap/commit/1625d9e8c97aa8beef0df0c00430c1c3d79190ab
accidentally deleted the required COMPONENTS keyword from
`cmake/FindDependencies.cmake`
when searching for Qt.
`find_package(Qt${QT_VERSION_MAJOR} ${COLMAP_FIND_TYPE} COMPONENTS
${COLMAP_QT_COMPONENTS})`

cmake/FindDependencies.cmake
@@ -421,7 +421,7 @@ if(GUI_ENABLED)
     if(${QT_VERSION_MAJOR} GREATER_EQUAL 6)
         list(APPEND COLMAP_QT_COMPONENTS OpenGLWidgets)
     endif()
-    find_package(Qt${QT_VERSION_MAJOR} ${COLMAP_FIND_TYPE} ${COLMAP_QT_COMPONENTS})
+    find_package(Qt${QT_VERSION_MAJOR} ${COLMAP_FIND_TYPE} COMPONENTS ${COLMAP_QT_COMPONENTS})
     message(STATUS "Found Qt")
     message(STATUS "  Module : ${Qt${QT_VERSION_MAJOR}Core_DIR}")
     message(STATUS "  Module : ${Qt${QT_VERSION_MAJOR}OpenGL_DIR}")