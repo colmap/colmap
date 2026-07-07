c3f082499cdc60cb965549c882a7d45d331f2f3b
Shaohui Liu <b1ueber2y@gmail.com>
2026-07-07 17:00:29 +0200

    strip INVLAID_TIMESTAMP binding due to a separate PR.

--- src/pycolmap/scene/constants.cc
+++ src/pycolmap/scene/constants.cc
@@ -14,5 +14,4 @@ void BindConstants(py::module& m) {
   m.attr("INVALID_POSE_PRIOR_ID") = colmap::kInvalidPosePriorId;
   m.attr("INVALID_SENSOR_ID") = colmap::kInvalidSensorId;
   m.attr("INVALID_DATA_ID") = colmap::kInvalidDataId;
-  m.attr("INVALID_TIMESTAMP") = colmap::kInvalidTimestamp;
 }
