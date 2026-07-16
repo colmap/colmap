ad877bb2b7cee66b40b2f516a7fcc44cbb028fa0
Shaohui Liu <b1ueber2y@gmail.com>
2026-07-16 16:38:15 +0200

    clang tidy.

--- src/colmap/scene/database_sqlite.cc
+++ src/colmap/scene/database_sqlite.cc
@@ -1464,9 +1464,9 @@ class SqliteDatabase : public Database {
 
     // Write NULL for camera1/camera2 if not set. The blob strings must outlive
     // sqlite3_step because they are bound with SQLITE_STATIC.
-    const std::string camera1_blob = BindOptionalCamera(
+    [[maybe_unused]] const std::string camera1_blob = BindOptionalCamera(
         sql_stmt_write_two_view_geometry_, two_view_geometry_ptr->camera1, 11);
-    const std::string camera2_blob = BindOptionalCamera(
+    [[maybe_unused]] const std::string camera2_blob = BindOptionalCamera(
         sql_stmt_write_two_view_geometry_, two_view_geometry_ptr->camera2, 12);
 
     SQLITE3_CALL(sqlite3_step(sql_stmt_write_two_view_geometry_));
