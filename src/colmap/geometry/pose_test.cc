2b8741cf7edabe80fe2a9b8505a027a5a48cd4bb
Johannes Schönberger <jsch@meta.com>
2026-08-07 18:48:16 +0200

    Restore platform-specific include dependencies

--- src/colmap/geometry/pose_test.cc
+++ src/colmap/geometry/pose_test.cc
@@ -29,6 +29,7 @@
 
 #include "colmap/geometry/pose.h"
 
+#include "colmap/math/math.h"
 #include "colmap/math/random.h"
 #include "colmap/math/random_eigen.h"
 #include "colmap/util/eigen_alignment.h"
