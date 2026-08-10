2e8e4c49922928ce886300ce0e6b712802b20cbb
Johannes Schönberger <jsch@meta.com>
2026-08-10 10:58:38 +0200

    Always load reconstruction images in image registrator

--- src/colmap/controllers/option_manager_test.cc
+++ src/colmap/controllers/option_manager_test.cc
@@ -135,7 +135,6 @@ TEST(OptionManager, WriteAndRead) {
   options_write.feature_extraction->max_image_size = 2048;
   options_write.feature_extraction->sift->max_num_features = 4096;
   options_write.mapper->min_num_matches = 20;
-  options_write.mapper->load_all_images = true;
 
   // Write to file
   options_write.Write(config_path);
@@ -160,8 +159,6 @@ TEST(OptionManager, WriteAndRead) {
             options_write.feature_extraction->sift->max_num_features);
   EXPECT_EQ(options_read.mapper->min_num_matches,
             options_write.mapper->min_num_matches);
-  EXPECT_EQ(options_read.mapper->load_all_images,
-            options_write.mapper->load_all_images);
 }
 
 TEST(OptionManager, ReRead) {
@@ -238,7 +235,6 @@ TEST(OptionManager, ParseWithOptions) {
   options.AddDatabaseOptions();
   options.AddImageOptions();
   options.AddFeatureExtractionOptions();
-  options.AddMapperOptions();
 
   const auto database_path = test_dir / "database.db";
   const auto image_path = test_dir / "images";
@@ -254,8 +250,6 @@ TEST(OptionManager, ParseWithOptions) {
       "1024",
       "--SiftExtraction.max_num_features",
       "2048",
-      "--Mapper.load_all_images",
-      "1",
   };
 
   std::vector<char*> argv;
@@ -271,7 +265,6 @@ TEST(OptionManager, ParseWithOptions) {
   EXPECT_EQ(*options.image_path, image_path);
   EXPECT_EQ(options.feature_extraction->max_image_size, 1024);
   EXPECT_EQ(options.feature_extraction->sift->max_num_features, 2048);
-  EXPECT_TRUE(options.mapper->load_all_images);
 }
 
 TEST(OptionManager, ParseWithProjectPath) {
