be9b50dbc5c7545ed058a2e7b0b9430643b6afc3
Zador-Pataki <zadorpataki14@gmail.com>
2026-09-22 14:16:02 +0200

    Keep optimize_points condition for scale initialization

--- src/colmap/estimators/global_positioning.cc
+++ src/colmap/estimators/global_positioning.cc
@@ -216,7 +216,7 @@ void GlobalPositioner::AddPoint3DToProblem(point3D_t point3D_id,
 
     double& scale = scales_.emplace_back(1);
 
-    if (options_.initialize_scales_from_geometry) {
+    if (options_.initialize_scales_from_geometry && options_.optimize_points) {
       const Eigen::Vector3d cam_from_point3D_translation =
           point3D.xyz - frame_centers_[image.FrameId()];
       scale = std::max(1e-5,
